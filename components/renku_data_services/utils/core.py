b222f0e7a5b508ed6bae516a98840c8444888f79
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2023-10-12 15:59:14 +0200

    address comments

--- components/renku_data_services/utils/core.py
+++ components/renku_data_services/utils/core.py
@@ -1,9 +1,14 @@
 """Shared utility functions."""
+import functools
+import os
 import ssl
 
 
+@functools.lru_cache(1)
 def get_ssl_context():
     """Get an SSL context supporting mounted custom certificates."""
     context = ssl.create_default_context()
-    context.load_verify_locations(capath="/usr/local/share/ca-certificates")
+    custom_cert_folder = os.environ.get("SSL_CERT_FOLDER", None)
+    if custom_cert_folder:
+        context.load_verify_locations(capath=custom_cert_folder)
     return context
