9922b4edecd97d8f7c65bcc0b37b91dbf7f66e2d
Johann-Michael Thiebaut <johann.thiebaut@gmail.com>
2023-11-03 09:17:43 +0100

    code suggestions

--- components/renku_data_services/utils/core.py
+++ components/renku_data_services/utils/core.py
@@ -2,6 +2,9 @@
 import functools
 import os
 import ssl
+from typing import Any, Set
+
+from deepmerge import Merger
 
 
 @functools.lru_cache(1)
@@ -12,3 +15,22 @@ def get_ssl_context():
     if custom_cert_file:
         context.load_verify_locations(cafile=custom_cert_file)
     return context
+
+
+def merge_api_specs(*args):
+    """Merges API spec files into a single one."""
+    merger = Merger(
+        type_strategies=[(list, "append_unique"), (dict, "merge"), (set, "union")],
+        fallback_strategies=["override"],
+        type_conflict_strategies=["override_if_not_empty"],
+    )
+
+    merged_spec: dict[str, Any]
+    merged_spec = functools.reduce(merger.merge, args, dict())
+
+    # Remove duplicate entries in `.servers`
+    server_urls: Set[str]
+    server_urls = set((obj["url"] for obj in merged_spec.get("servers", [])))
+    merged_spec["servers"] = [dict(url=url) for url in server_urls]
+
+    return merged_spec
