c001dc3c0950af0e503da0db9cbf3e7160330372
Flora Thiebaut <flora.thiebaut.91@gmail.com>
2023-11-06 14:12:42 +0100

    added more tests

--- test/bases/renku_data_services/data_api/test_user_preferences.py
+++ test/bases/renku_data_services/data_api/test_user_preferences.py
@@ -1,3 +1,4 @@
+import json
 from test.bases.renku_data_services.data_api.utils import create_user_preferences
 from typing import Any, Dict
 
@@ -30,6 +31,93 @@ async def test_get_user_preferences(test_client: SanicASGITestClient, valid_add_
 
     _, res = await test_client.get(
         "/api/data/user_preferences",
+        headers={"Authorization": "bearer test"},
+    )
+
+    assert res.status_code == 200
+    assert res.json is not None
+    assert res.json.get("user_id") == "some-id"
+    assert res.json.get("pinned_projects") is not None
+    assert len(res.json["pinned_projects"].get("project_slugs")) == 1
+    project_slugs = res.json["pinned_projects"]["project_slugs"]
+    assert project_slugs[0] == "user.1/first-project"
+
+
+@pytest.mark.asyncio
+async def test_post_user_preferences_pinned_projects(
+    test_client: SanicASGITestClient, valid_add_pinned_project_payload: Dict[str, Any]
+):
+    _, res = await create_user_preferences(test_client, valid_add_pinned_project_payload)
+    assert res.status_code == 200
+
+    _, res = await test_client.post(
+        "/api/data/user_preferences/pinned_projects",
+        headers={"Authorization": "bearer test"},
+        data=json.dumps(dict(project_slug="user.2/second-project")),
+    )
+
+    assert res.status_code == 200
+    assert res.json is not None
+    assert res.json.get("user_id") == "some-id"
+    assert res.json.get("pinned_projects") is not None
+    assert len(res.json["pinned_projects"].get("project_slugs")) == 2
+    project_slugs = res.json["pinned_projects"]["project_slugs"]
+    assert project_slugs[0] == "user.1/first-project"
+    assert project_slugs[1] == "user.2/second-project"
+
+
+@pytest.mark.asyncio
+async def test_post_user_preferences_pinned_projects_existing(
+    test_client: SanicASGITestClient, valid_add_pinned_project_payload: Dict[str, Any]
+):
+    _, res = await create_user_preferences(test_client, valid_add_pinned_project_payload)
+    assert res.status_code == 200
+
+    _, res = await test_client.post(
+        "/api/data/user_preferences/pinned_projects",
+        headers={"Authorization": "bearer test"},
+        data=json.dumps(valid_add_pinned_project_payload),
+    )
+
+    assert res.status_code == 200
+    assert res.json is not None
+    assert res.json.get("user_id") == "some-id"
+    assert res.json.get("pinned_projects") is not None
+    assert len(res.json["pinned_projects"].get("project_slugs")) == 1
+    project_slugs = res.json["pinned_projects"]["project_slugs"]
+    assert project_slugs[0] == "user.1/first-project"
+
+
+@pytest.mark.asyncio
+async def test_delete_user_preferences_pinned_projects(
+    test_client: SanicASGITestClient, valid_add_pinned_project_payload: Dict[str, Any]
+):
+    _, res = await create_user_preferences(test_client, valid_add_pinned_project_payload)
+    assert res.status_code == 200
+
+    _, res = await test_client.delete(
+        "/api/data/user_preferences/pinned_projects",
+        params=dict(project_slug="user.1/first-project"),
+        headers={"Authorization": "bearer test"},
+    )
+
+    assert res.status_code == 200
+    assert res.json is not None
+    assert res.json.get("user_id") == "some-id"
+    assert res.json.get("pinned_projects") is not None
+    assert len(res.json["pinned_projects"].get("project_slugs")) == 0
+
+
+@pytest.mark.asyncio
+async def test_delete_user_preferences_pinned_projects_unknown(
+    test_client: SanicASGITestClient, valid_add_pinned_project_payload: Dict[str, Any]
+):
+    _, res = await create_user_preferences(test_client, valid_add_pinned_project_payload)
+    assert res.status_code == 200
+
+    _, res = await test_client.delete(
+        "/api/data/user_preferences/pinned_projects",
+        params=dict(project_slug="user.2/second-project"),
         headers={"Authorization": "bearer test"},
     )
 
