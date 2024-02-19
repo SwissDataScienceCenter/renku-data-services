887e4d107d600ebf6ab836a99947f4b927091bdb
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2024-02-19 14:53:08 +0100

    fix environment id check

--- components/renku_data_services/session/db.py
+++ components/renku_data_services/session/db.py
@@ -40,8 +40,7 @@ class SessionRepository:
             environment = res.one_or_none()
             if environment is None:
                 raise errors.MissingResourceError(
-                    message=f"Session environment with id '{environment_id}' \
-                        does not exist or you do not have access to it."
+                    message=f"Session environment with id '{environment_id}' does not exist or you do not have access to it."  # noqa: E501
                 )
             return environment.dump()
 
@@ -207,8 +206,7 @@ class SessionRepository:
                     environment = res.one_or_none()
                     if environment is None:
                         raise errors.MissingResourceError(
-                            message=f"Session environment with id '{environment_id}' \
-                                does not exist or you do not have access to it."
+                            message=f"Session environment with id '{environment_id}' does not exist or you do not have access to it."  # noqa: E501
                         )
 
                 launcher = schemas.SessionLauncherORM.load(launcher_model)
@@ -228,8 +226,7 @@ class SessionRepository:
                 launcher = res.one_or_none()
                 if launcher is None:
                     raise errors.MissingResourceError(
-                        message=f"Session launcher with id '{launcher_id}' \
-                            does not exist or you do not have access to it."
+                        message=f"Session launcher with id '{launcher_id}' does not exist or you do not have access to it."  # noqa: E501
                     )
 
                 authorized = await self.project_authz.has_permission(
@@ -237,6 +234,17 @@ class SessionRepository:
                 )
                 if not authorized:
                     raise errors.Unauthorized(message="You do not have the required permissions for this operation.")
+
+                environment_id = kwargs.get("environment_id")
+                if environment_id is not None:
+                    res = await session.scalars(
+                        select(schemas.SessionEnvironmentORM).where(schemas.SessionEnvironmentORM.id == environment_id)
+                    )
+                    environment = res.one_or_none()
+                    if environment is None:
+                        raise errors.MissingResourceError(
+                            message=f"Session environment with id '{environment_id}' does not exist or you do not have access to it."  # noqa: E501
+                        )
 
                 for key, value in kwargs.items():
                     # NOTE: Only ``name``, ``description``, ``environment_kind``,
@@ -270,112 +278,3 @@ class SessionRepository:
                     return
 
                 await session.delete(launcher)
-
-    # async def get_sessions(self, user: base_models.APIUser) -> list[models.Session]:
-    #     """Get all sessions from the database."""
-    #     user_id = user.id if user.is_authenticated else MemberQualifier.ALL
-    #     # NOTE: without the line below mypy thinks user_id can be None
-    #     user_id = user_id if user_id is not None else MemberQualifier.ALL
-    #     project_ids = await self.project_authz.get_user_projects(requested_by=user, user_id=user_id, scope=Scope.READ)
-
-    #     async with self.session_maker() as session:
-    #         statement = select(schemas.SessionORM)
-    #         statement = statement.where(schemas.SessionORM.project_id.in_(project_ids))
-    #         statement = statement.order_by(schemas.SessionORM.creation_date.desc())
-    #         result = await session.execute(statement)
-    #         sessions_orm = result.scalars().all()
-
-    #         return [p.dump() for p in sessions_orm]
-
-    # async def get_session(self, user: base_models.APIUser, session_id: str) -> models.Session:
-    #     """Get one session from the database."""
-    #     async with self.session_maker() as session:
-    #         statement = select(schemas.SessionORM).where(schemas.SessionORM.id == session_id)
-    #         result = await session.execute(statement)
-    #         session_orm = result.scalars().first()
-
-    #         if session_orm is None:
-    #             raise errors.MissingResourceError(message=f"Session with id '{session_id}' does not exist.")
-
-    #         authorized = await self.project_authz.has_permission(
-    #             user=user, project_id=session_orm.project_id, scope=Scope.READ
-    #         )
-    #         if not authorized:
-    #             raise errors.MissingResourceError(
-    #                 message=f"Session with id '{session_id}' does not exist or you do not have access to it."
-    #             )
-
-    #         return session_orm.dump()
-
-    # async def insert_session(self, user: base_models.APIUser, session: models.Session) -> models.Session:
-    #     """Insert a new session entry."""
-    #     authorized = await self.project_authz.has_permission(
-    #         user=user, project_id=session.project_id, scope=Scope.WRITE
-    #     )
-    #     if not authorized:
-    #         raise errors.MissingResourceError(
-    #             message=f"Project with id '{session.project_id}' does not exist or you do not have access to it."
-    #         )
-
-    #     session_orm = schemas.SessionORM.load(session)
-    #     session_orm.creation_date = datetime.now(timezone.utc).replace(microsecond=0)
-    #     session_orm.created_by = user.id
-
-    #     async with self.session_maker() as database_session:
-    #         async with database_session.begin():
-    #             database_session.add(session_orm)
-
-    #             if session_orm.id is None:
-    #                 raise errors.BaseError(detail="The created session does not have an ID but it should.")
-
-    #     return session_orm.dump()
-
-    # async def update_session(self, user: base_models.APIUser, session_id: str, **payload) -> models.Session:
-    #     """Update a session entry."""
-    #     async with self.session_maker() as session:
-    #         async with session.begin():
-    #             result = await session.execute(select(schemas.SessionORM).where(schemas.SessionORM.id == session_id))
-    #             sessions_orm = result.one_or_none()
-
-    #             if sessions_orm is None:
-    #                 raise errors.MissingResourceError(
-    #                     message=f"Session with id '{session_id}' does not exist or you do not have access to it."
-    #                 )
-    #             session_orm = sessions_orm[0]
-
-    #             authorized = await self.project_authz.has_permission(
-    #                 user=user, project_id=session_orm.project_id, scope=Scope.WRITE
-    #             )
-    #             if not authorized:
-    #                 raise errors.MissingResourceError(
-    #                     message=f"Session with id '{session_id}' does not exist or you do not have access to it."
-    #                 )
-
-    #             for key, value in payload.items():
-    #                 # NOTE: Only ``name``, ``description``, and ``environment_id`` can be edited
-    #                 if key in ["name", "description", "environment_id"]:
-    #                     setattr(session_orm, key, value)
-
-    #             # NOTE: Triggers validation before the transaction saves data
-    #             return session_orm.dump()
-
-    # async def delete_session(self, user: base_models.APIUser, session_id: str) -> None:
-    #     """Delete a cloud session entry."""
-    #     async with self.session_maker() as session:
-    #         async with session.begin():
-    #             result = await session.execute(select(schemas.SessionORM).where(schemas.SessionORM.id == session_id))
-    #             sessions_orm = result.one_or_none()
-
-    #             if sessions_orm is None:
-    #                 return
-    #             session_orm = sessions_orm[0]
-
-    #             authorized = await self.project_authz.has_permission(
-    #                 user=user, project_id=session_orm.project_id, scope=Scope.DELETE
-    #             )
-    #             if not authorized:
-    #                 raise errors.MissingResourceError(
-    #                     message=f"Session with id '{session_id}' does not exist or you do not have access to it."
-    #                 )
-
-    #             await session.delete(session_orm)
