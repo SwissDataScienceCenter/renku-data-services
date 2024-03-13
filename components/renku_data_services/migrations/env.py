1879cbc121dea113ed971cc12cf2d1a7d138fcc7
Tasko Olevski <olevski90@gmail.com>
2024-03-13 10:39:52 +0100

    chore: combine alembic version tables (#144)
    Co-authored-by: Ralf Grubenmann <ralf.grubenmann@gmail.com>

--- components/renku_data_services/migrations/env.py
+++ components/renku_data_services/migrations/env.py
@@ -1,78 +1,32 @@
-"""Custom migrations env file to support modular migrations."""
+"""Database migrations for Alembic."""
+
+from renku_data_services.authz.orm import BaseORM as authz
+from renku_data_services.crc.orm import BaseORM as crc
+from renku_data_services.project.orm import BaseORM as project
+from renku_data_services.storage.orm import BaseORM as storage
+from renku_data_services.user_preferences.orm import BaseORM as user_preferences
+from renku_data_services.users.orm import BaseORM as users
+from renku_data_services.message_queue.orm import BaseORM as events
+from renku_data_services.migrations.utils import run_migrations
 from alembic import context
-from sqlalchemy import MetaData, NullPool, create_engine
-from sqlalchemy.schema import CreateSchema
+from logging.config import fileConfig
 
-from renku_data_services.db_config import DBConfig
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
 
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
 
-def include_object_factory(schema: str):
-    """Filter only objects for the current database schema to be included."""
-
-    def _include_object(object, name, type_, reflected, compare_to):
-        if type_ == "table" and object.schema != schema:
-            return False
-        else:
-            return True
-
-    return _include_object
-
-
-def run_migrations_offline(target_metadata, sync_sqlalchemy_url: str) -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    engine = create_engine(sync_sqlalchemy_url, poolclass=NullPool)
-    with engine.connect() as conn:
-        context.configure(
-            connection=conn,
-            target_metadata=target_metadata,
-            literal_binds=True,
-            dialect_opts={"paramstyle": "named"},
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
-
-
-def run_migrations_online(target_metadata, sync_sqlalchemy_url: str) -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    engine = create_engine(sync_sqlalchemy_url, poolclass=NullPool)
-    with engine.connect() as conn:
-        context.configure(
-            connection=conn,
-            target_metadata=target_metadata,
-            version_table_schema=target_metadata.schema,
-            include_schemas=True,
-            include_object=include_object_factory(target_metadata.schema),
-        )
-
-        conn.execute(CreateSchema(target_metadata.schema, if_not_exists=True))
-
-        with context.begin_transaction():
-            context.run_migrations()
-
-
-def run_migrations(metadata: MetaData):
-    """Run migrations for a specific base model class."""
-    # this is the Alembic Config object, which provides
-    # access to the values within the .ini file in use.
-    db_config = DBConfig.from_env()
-    sync_sqlalchemy_url = db_config.conn_url(async_client=False)
-    if context.is_offline_mode():
-        run_migrations_offline(metadata, sync_sqlalchemy_url)
-    else:
-        run_migrations_online(metadata, sync_sqlalchemy_url)
+all_metadata = [
+    authz.metadata,
+    crc.metadata,
+    project.metadata,
+    storage.metadata,
+    user_preferences.metadata,
+    users.metadata,
+    events.metadata,
+]
+run_migrations(all_metadata)
---
+++ components/renku_data_services/migrations/utils.py
@@ -0,0 +1,141 @@
+"""Custom migrations env file to support modular migrations."""
+from typing import Sequence
+
+from alembic import context
+from sqlalchemy import Connection, MetaData, NullPool, create_engine
+from sqlalchemy.schema import CreateSchema
+from sqlalchemy.sql import text
+
+from renku_data_services.db_config import DBConfig
+
+
+def include_object(obj, name, type_, reflected, compare_to):
+    """Prevents from alembic migrating the alembic_version tables."""
+    if type_ == "table" and name == "alembic_version":
+        return False
+    return True
+
+
+def combine_version_tables(conn: Connection, metadata_schema: str | None):
+    """Used to combine all alembic version tables into one."""
+    schemas = {
+        # NOTE: These are the revisions that each schema will be when the version table is moved
+        # in all other cases this function will do nothing.
+        "authz": "748ed0f3439f",
+        "projects": "7c08ed2fb79d",
+        "resource_pools": "5403953f654f",
+        "storage": "61a4d72981cf",
+        "users": "3b30da432a76",
+        "user_preferences": "6eccd7d4e3ed",
+        "events": "4c425d8889b6",
+    }
+    if not metadata_schema:
+        return
+    with conn.begin_nested():
+        rev = schemas.get(metadata_schema)
+        if not rev:
+            # The table revision is not the correct for merging version tables
+            return
+        version_table_exists_row = conn.execute(
+            text(f"SELECT to_regclass('{metadata_schema}.alembic_version')")
+        ).fetchone()
+        if not version_table_exists_row:
+            # The old version table or schema does not exist
+            return
+        version_table_exists = version_table_exists_row[0]
+        if not version_table_exists:
+            # The old version table or schema does not exist
+            return
+        last_migration_row = conn.execute(
+            text(f"SELECT version_num from {metadata_schema}.alembic_version")  # nosec B608
+        ).fetchone()
+        if not last_migration_row:
+            # The version table exists but it has not data
+            return
+        last_migration_rev = last_migration_row[0]
+        if last_migration_rev != rev:
+            # The version table has data but it does not match the revision required for migration
+            return
+        conn.execute(text(f"LOCK TABLE {metadata_schema}.alembic_version IN ACCESS EXCLUSIVE MODE"))
+        conn.execute(text("LOCK TABLE common.alembic_version IN ACCESS EXCLUSIVE MODE"))
+        conn.execute(
+            text(
+                "CREATE TABLE IF NOT EXISTS common.alembic_version "
+                f"(LIKE {metadata_schema}.alembic_version INCLUDING ALL)"
+            )
+        )
+        conn.execute(text("INSERT INTO common.alembic_version(version_num) VALUES (:rev)").bindparams(rev=rev))
+        conn.execute(text(f"DROP TABLE IF EXISTS {metadata_schema}.alembic_version"))
+
+
+def run_migrations_offline(target_metadata: Sequence[MetaData], sync_sqlalchemy_url: str) -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    engine = create_engine(sync_sqlalchemy_url, poolclass=NullPool)
+    with engine.connect() as conn:
+        context.configure(
+            connection=conn,
+            target_metadata=target_metadata,
+            literal_binds=True,
+            dialect_opts={"paramstyle": "named"},
+            include_schemas=True,
+            include_object=include_object,
+            version_table_schema="common",
+        )
+        conn.execute(CreateSchema("common", if_not_exists=True))
+        for m in target_metadata:
+            conn.execute(CreateSchema(m.schema, if_not_exists=True))
+            combine_version_tables(conn, m.schema)
+
+        with context.begin_transaction():
+            context.get_context()._ensure_version_table()
+            conn.execute(text("LOCK TABLE common.alembic_version IN ACCESS EXCLUSIVE MODE"))
+            context.run_migrations()
+
+
+def run_migrations_online(target_metadata: Sequence[MetaData], sync_sqlalchemy_url: str) -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    engine = create_engine(sync_sqlalchemy_url, poolclass=NullPool)
+    with engine.connect() as conn:
+        context.configure(
+            connection=conn,
+            target_metadata=target_metadata,
+            include_schemas=True,
+            include_object=include_object,
+            version_table_schema="common",
+        )
+        conn.execute(CreateSchema("common", if_not_exists=True))
+        for m in target_metadata:
+            conn.execute(CreateSchema(m.schema, if_not_exists=True))
+            combine_version_tables(conn, m.schema)
+
+        with context.begin_transaction():
+            context.get_context()._ensure_version_table()
+            conn.execute(text("LOCK TABLE common.alembic_version IN ACCESS EXCLUSIVE MODE"))
+            context.run_migrations()
+
+
+def run_migrations(metadata: Sequence[MetaData]):
+    """Run migrations for a specific base model class."""
+    # this is the Alembic Config object, which provides
+    # access to the values within the .ini file in use.
+    db_config = DBConfig.from_env()
+    sync_sqlalchemy_url = db_config.conn_url(async_client=False)
+    if context.is_offline_mode():
+        run_migrations_offline(metadata, sync_sqlalchemy_url)
+    else:
+        run_migrations_online(metadata, sync_sqlalchemy_url)
