40ce82e8843119da7a33001c38161f0f453bfa82
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2024-04-05 09:49:23 +0200

    fix middleware and tests

--- components/renku_data_services/utils/middleware.py
+++ components/renku_data_services/utils/middleware.py
@@ -7,5 +7,5 @@ from renku_data_services import errors
 
 async def validate_null_byte(request: Request):
     """Validate that a request does not contain a null byte."""
-    if b"\x00" in request.body:
+    if b"\u0000" in request.body:
         raise errors.ValidationError(message="Null byte found in request")
