443aef4ed1aae2d28c3f66389cf7fd08da0eb810
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2024-04-08 10:33:04 +0200

    chore: commit updated avro schemas

--- components/renku_data_services/message_queue/avro_models/io/renku/events/v1/__init__.py
+++ components/renku_data_services/message_queue/avro_models/io/renku/events/v1/__init__.py
@@ -5,8 +5,8 @@ from renku_data_services.message_queue.avro_models.io.renku.events.v1.project_au
 from renku_data_services.message_queue.avro_models.io.renku.events.v1.visibility import Visibility
 from renku_data_services.message_queue.avro_models.io.renku.events.v1.project_authorization_added import ProjectAuthorizationAdded
 from renku_data_services.message_queue.avro_models.io.renku.events.v1.project_removed import ProjectRemoved
-from renku_data_services.message_queue.avro_models.io.renku.events.v1.project_created import ProjectCreated
+from renku_data_services.message_queue.avro_models.io.renku.events.v1.project_updated import ProjectUpdated
 from renku_data_services.message_queue.avro_models.io.renku.events.v1.user_added import UserAdded
 from renku_data_services.message_queue.avro_models.io.renku.events.v1.user_removed import UserRemoved
 from renku_data_services.message_queue.avro_models.io.renku.events.v1.user_updated import UserUpdated
-from renku_data_services.message_queue.avro_models.io.renku.events.v1.project_updated import ProjectUpdated
+from renku_data_services.message_queue.avro_models.io.renku.events.v1.project_created import ProjectCreated
