e5bae081c859c4b724ce3d9cb91ca04ac967bda9
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2024-05-06 10:02:10 +0000

    add tests for connected services

---
+++ test/bases/renku_data_services/data_api/test_connected_services.py
@@ -0,0 +1,208 @@
+"""Tests for connected services blueprints."""
+
+from typing import Any
+from urllib.parse import parse_qs, urlparse
+
+import pytest
+from sanic_testing.testing import SanicASGITestClient
+
+
+@pytest.fixture
+def create_oauth2_provider(sanic_client: SanicASGITestClient, admin_headers):
+    async def create_oauth2_provider_helper(provider_id: str, **payload) -> dict[str, Any]:
+        payload = payload.copy()
+        payload.update({"id": provider_id})
+        payload["kind"] = payload.get("kind") or "gitlab"
+        payload["client_id"] = payload.get("client_id") or "some-client-id"
+        payload["client_secret"] = payload.get("client_secret") or "some-client-secret"
+        payload["display_name"] = payload.get("display_name") or "my oauth2 application"
+        payload["scope"] = payload.get("scope") or "api"
+        payload["url"] = payload.get("url") or "https://example.org"
+        _, res = await sanic_client.post("/api/data/oauth2/admin/providers", headers=admin_headers, json=payload)
+
+        assert res.status_code == 201, res.text
+        assert res.json is not None
+        return res.json
+
+    return create_oauth2_provider_helper
+
+
+@pytest.mark.asyncio
+async def test_get_all_oauth2_providers(
+    sanic_client: SanicASGITestClient, unauthorized_headers, create_oauth2_provider
+):
+    await create_oauth2_provider("provider_1")
+    await create_oauth2_provider("provider_2")
+    await create_oauth2_provider("provider_3")
+
+    _, res = await sanic_client.get("/api/data/oauth2/providers", headers=unauthorized_headers)
+
+    assert res.status_code == 200, res.text
+    assert res.json is not None
+    providers = res.json
+    assert {p["id"] for p in providers} == {
+        "provider_1",
+        "provider_2",
+        "provider_3",
+    }
+
+
+@pytest.mark.asyncio
+async def test_get_oauth2_provider(sanic_client: SanicASGITestClient, unauthorized_headers, create_oauth2_provider):
+    provider = await create_oauth2_provider(
+        "provider_1",
+        display_name="Some external service",
+        url="https://my-example.org",
+    )
+    provider_id = provider["id"]
+
+    _, res = await sanic_client.get(f"/api/data/oauth2/providers/{provider_id}", headers=unauthorized_headers)
+
+    assert res.status_code == 200, res.text
+    assert res.json is not None
+    assert res.json.get("id") == "provider_1"
+    assert res.json.get("display_name") == "Some external service"
+    assert res.json.get("url") == "https://my-example.org"
+
+
+@pytest.mark.asyncio
+async def test_post_oauth2_provider(sanic_client: SanicASGITestClient, admin_headers):
+    payload = {
+        "id": "some-provider",
+        "kind": "gitlab",
+        "client_id": "some-client-id",
+        "client_secret": "some-client-secret",
+        "display_name": "Some external service",
+        "scope": "api",
+        "url": "https://example.org",
+    }
+
+    _, res = await sanic_client.post("/api/data/oauth2/admin/providers", headers=admin_headers, json=payload)
+
+    assert res.status_code == 201, res.text
+    assert res.json is not None
+    assert res.json.get("id") == "some-provider"
+    assert res.json.get("kind") == "gitlab"
+    assert res.json.get("client_id") == "some-client-id"
+    assert "client_secret" not in res.json
+    assert res.json.get("display_name") == "Some external service"
+    assert res.json.get("scope") == "api"
+    assert res.json.get("url") == "https://example.org"
+
+
+@pytest.mark.asyncio
+async def test_post_oauth2_provider_unauthorized(sanic_client: SanicASGITestClient, user_headers):
+    payload = {
+        "id": "some-provider",
+        "kind": "gitlab",
+        "client_id": "some-client-id",
+        "client_secret": "some-client-secret",
+        "display_name": "Some external service",
+        "scope": "api",
+        "url": "https://example.org",
+    }
+
+    _, res = await sanic_client.post("/api/data/oauth2/admin/providers", headers=user_headers, json=payload)
+
+    assert res.status_code == 401, res.text
+
+
+@pytest.mark.asyncio
+async def test_patch_oauth2_provider(sanic_client: SanicASGITestClient, admin_headers, create_oauth2_provider):
+    provider = await create_oauth2_provider("provider_1")
+    provider_id = provider["id"]
+
+    payload = {
+        "display_name": "New display name",
+        "scope": "read write",
+        "url": "https://my-new-example.org",
+    }
+
+    _, res = await sanic_client.patch(
+        f"/api/data/oauth2/admin/providers/{provider_id}", headers=admin_headers, json=payload
+    )
+
+    assert res.status_code == 200, res.text
+    assert res.json is not None
+    assert res.json.get("display_name") == "New display name"
+    assert res.json.get("scope") == "read write"
+    assert res.json.get("url") == "https://my-new-example.org"
+
+
+@pytest.mark.asyncio
+async def test_patch_oauth2_provider_unauthorized(
+    sanic_client: SanicASGITestClient, user_headers, create_oauth2_provider
+):
+    provider = await create_oauth2_provider("provider_1")
+    provider_id = provider["id"]
+
+    payload = {
+        "display_name": "New display name",
+        "scope": "read write",
+        "url": "https://my-new-example.org",
+    }
+
+    _, res = await sanic_client.patch(
+        f"/api/data/oauth2/admin/providers/{provider_id}", headers=user_headers, json=payload
+    )
+
+    assert res.status_code == 401, res.text
+
+
+@pytest.mark.asyncio
+async def test_delete_oauth2_provider(sanic_client: SanicASGITestClient, admin_headers, create_oauth2_provider):
+    provider = await create_oauth2_provider("provider_1")
+    provider_id = provider["id"]
+
+    _, res = await sanic_client.delete(f"/api/data/oauth2/admin/providers/{provider_id}", headers=admin_headers)
+
+    assert res.status_code == 204, res.text
+
+
+@pytest.mark.asyncio
+async def test_delete_oauth2_provider_unauthorized(
+    sanic_client: SanicASGITestClient, user_headers, create_oauth2_provider
+):
+    provider = await create_oauth2_provider("provider_1")
+    provider_id = provider["id"]
+
+    _, res = await sanic_client.delete(f"/api/data/oauth2/admin/providers/{provider_id}", headers=user_headers)
+
+    assert res.status_code == 401, res.text
+
+
+@pytest.mark.asyncio
+async def test_start_oauth2_authorization_flow(sanic_client: SanicASGITestClient, user_headers, create_oauth2_provider):
+    provider = await create_oauth2_provider("provider_1")
+    provider_id = provider["id"]
+
+    _, res = await sanic_client.get(f"/api/data/oauth2/providers/{provider_id}/authorize", headers=user_headers)
+
+    assert res.status_code == 302, res.text
+    assert "location" in res.headers
+    location = urlparse(res.headers["location"])
+    assert location.scheme == "https"
+    assert location.netloc == "example.org"
+    assert location.path == "/oauth/authorize"
+    assert location.query != ""
+    query = parse_qs(location.query)
+    assert query.get("client_id", [None])[0] == "some-client-id"
+    assert query.get("redirect_uri", [None])[0] is not None
+    redirect_uri = urlparse(query.get("redirect_uri", [None])[0])
+    assert redirect_uri.path == "/api/data/oauth2/callback"
+    assert query.get("response_type", [None])[0] == "code"
+    assert query.get("scope", [None])[0] == "api"
+    assert query.get("state", [None])[0] != ""
+
+    _, res = await sanic_client.get("/api/data/oauth2/connections", headers=user_headers)
+
+    assert res.status_code == 200, res.text
+    assert res.json is not None
+    connection = None
+    for conn in res.json:
+        if conn["provider_id"] == provider_id:
+            connection = conn
+            break
+    assert connection is not None
+    assert connection.get("id") != ""
+    assert connection.get("status") == "pending"
