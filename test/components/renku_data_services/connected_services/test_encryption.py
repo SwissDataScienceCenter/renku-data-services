2a8f481361bd64a479b047af00c676f3f35d9aff
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2024-05-13 13:07:48 +0000

    address comment on test

--- test/components/renku_data_services/connected_services/test_encryption.py
+++ test/components/renku_data_services/connected_services/test_encryption.py
@@ -1,8 +1,5 @@
 """Tests for the encryption in connected services."""
 
-import json
-from uuid import uuid4
-
 import pytest
 from sqlalchemy import select
 
@@ -11,33 +8,6 @@ from renku_data_services.base_models import APIUser
 from renku_data_services.connected_services import apispec
 from renku_data_services.connected_services import orm as schemas
 from renku_data_services.utils.cryptography import decrypt_string
-
-
-@pytest.fixture
-def admin_api_user() -> APIUser:
-    id = str(uuid4())
-    full_name = "Some Admin"
-    first_name = "Some"
-    last_name = "Admin"
-    email = "some-admin@example.org"
-    is_admin = True
-    return APIUser(
-        is_admin=is_admin,
-        id=id,
-        full_name=full_name,
-        # The dummy authentication client in the tests will parse the access token to create
-        # the same APIUser as this when it receives this json-formatted access token
-        access_token=json.dumps(
-            {
-                "id": id,
-                "first_name": first_name,
-                "last_name": last_name,
-                "email": email,
-                "is_admin": is_admin,
-                "full_name": full_name,
-            }
-        ),
-    )
 
 
 def test_token_encryption(app_config: Config):
@@ -61,7 +31,7 @@ def test_token_encryption(app_config: Config):
 
 
 @pytest.mark.asyncio
-async def test_client_secret_encryption(app_config: Config, admin_api_user: APIUser):
+async def test_client_secret_encryption(app_config: Config, admin_user: APIUser):
     connected_services_repo = app_config.connected_services_repo
     new_client = apispec.ProviderPost(
         id="provider",
@@ -73,7 +43,7 @@ async def test_client_secret_encryption(app_config: Config, admin_api_user: APIU
         url="https://example.org",
     )
 
-    client = await connected_services_repo.insert_oauth2_client(user=admin_api_user, new_client=new_client)
+    client = await connected_services_repo.insert_oauth2_client(user=admin_user, new_client=new_client)
 
     assert client is not None
     assert client.id == new_client.id
@@ -87,7 +57,7 @@ async def test_client_secret_encryption(app_config: Config, admin_api_user: APIU
     assert stored_client.client_secret != "CLIENT_SECRET"  # nosec
 
     decrypted_secret = decrypt_string(
-        connected_services_repo.encryption_key, admin_api_user.id, stored_client.client_secret
+        connected_services_repo.encryption_key, admin_user.id, stored_client.client_secret
     )
 
     assert decrypted_secret == "CLIENT_SECRET"  # nosec
