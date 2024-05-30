e5d61cfa698bc6f3daf15a1b3aba25d1dd7bb78f
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2024-05-30 21:47:43 +0200

    squashme: address comments

--- bases/renku_data_services/background_jobs/main.py
+++ bases/renku_data_services/background_jobs/main.py
@@ -13,30 +13,40 @@ from renku_data_services.migrations.core import run_migrations_for_app
 logging.basicConfig(level=logging.INFO)
 
 
+async def short_period_sync() -> None:
+    """Perform synchronizations and jobs that should occur more often."""
+    config = SyncConfig.from_env()
+    run_migrations_for_app("common")
+    await bootstrap_user_namespaces(config)
+    await config.syncer.events_sync(config.kc_api)
+    await sync_admins_from_keycloak(config.kc_api, Authz(config.authz_config))
+
+
+async def long_period_sync() -> None:
+    """Perform synchronizations and jobs that can occur more rarely."""
+    config = SyncConfig.from_env()
+    run_migrations_for_app("common")
+    await config.syncer.users_sync(config.kc_api)
+    await sync_admins_from_keycloak(config.kc_api, Authz(config.authz_config))
+
+
 async def main() -> None:
     """Synchronize data from Keycloak and the user database."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "tasks",
-        nargs="+",
-        choices=["keycloak_events_sync", "keycloak_users_sync", "keycloak_admins_sync", "user_namespaces_bootstrap"],
+
+    parser = argparse.ArgumentParser(prog="Data Service Background Jobs")
+    subparsers = parser.add_subparsers(help="Background job to run")
+
+    short_period_sync_parser = subparsers.add_parser(
+        "short_period_sync", help="Perform background jobs that need to occur more often"
     )
-    config = SyncConfig.from_env()
+    short_period_sync_parser.set_defaults(func=short_period_sync)
+    long_period_sync_parser = subparsers.add_parser(
+        "long_period_sync", help="Perform background jobs that need to occur at a longer period"
+    )
+    long_period_sync_parser.set_defaults(func=long_period_sync)
+
     args = parser.parse_args()
-    for task in args.tasks:
-        logging.info(f"Starting task {task}")
-        match task:
-            case "keycloak_events_sync":
-                await config.syncer.events_sync(config.kc_api)
-            case "keycloak_users_sync":
-                await config.syncer.users_sync(config.kc_api)
-            case "keycloak_admins_sync":
-                await sync_admins_from_keycloak(config.kc_api, Authz(config.authz_config))
-            case "user_namespaces_bootstrap":
-                run_migrations_for_app("common")
-                await bootstrap_user_namespaces(config)
-            case _:
-                raise ValueError(f"Found an unexpected background job task name '{task}'")
+    await args.func()
 
 
 if __name__ == "__main__":
