b19e9d333233be23bc3b0b9914ee861df0a44cc9
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2024-06-03 14:40:24 +0200

    feat: API endpoint for repository permissions in connected services (#197)
    Add new API endpoint to let users check git repository permissions with connected services.
    
    * `/repositories/<repository_url>` can be used to check `pull`/`push` permissions
    * `/repositories/<repository_url>/probe` can be used to check if a repository is publicly accessible

---
+++ components/renku_data_services/repositories/utils.py
@@ -0,0 +1,13 @@
+"""Utilities for connected services."""
+
+import httpx
+
+
+async def probe_repository(repository_url: str) -> bool:
+    """Probe a repository to check if it is publicly available."""
+    async with httpx.AsyncClient() as client:
+        url = f"{repository_url}/info/refs?service=git-upload-pack"
+        res = await client.get(url=url)
+        if res.status_code != 200:
+            return False
+        return bool(res.headers.get("Content-Type") == "application/x-git-upload-pack-advertisement")
