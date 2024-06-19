b818f803ddcb1478927b70a08884df0c06311a7a
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2024-06-19 11:03:48 +0000

    add minimum_role param to get namespaces

--- components/renku_data_services/namespace/db.py
+++ components/renku_data_services/namespace/db.py
@@ -288,11 +288,18 @@ class GroupRepository:
         return group.dump()
 
     async def get_namespaces(
-        self, user: base_models.APIUser, pagination: PaginationRequest
+        self, user: base_models.APIUser, pagination: PaginationRequest, minimum_role: apispec.GroupRole | None = None
     ) -> tuple[list[models.Namespace], int]:
         """Get all namespaces."""
+        minimum_role = Role.from_group_role(minimum_role) if minimum_role is not None else None
+        scope = Scope.READ
+        if minimum_role == Role.EDITOR:
+            scope = Scope.WRITE
+        if minimum_role == Role.OWNER:
+            scope = Scope.DELETE
+
         async with self.session_maker() as session, session.begin():
-            group_ids = await self.authz.resources_with_permission(user, user.id, ResourceType.group, Scope.READ)
+            group_ids = await self.authz.resources_with_permission(user, user.id, ResourceType.group, scope)
             group_ns_stmt = select(schemas.NamespaceORM).where(schemas.NamespaceORM.group_id.in_(group_ids))
             output = []
             if pagination.page == 1:
--- test/bases/renku_data_services/data_api/test_namespaces.py
+++ test/bases/renku_data_services/data_api/test_namespaces.py
@@ -21,6 +21,73 @@ async def test_list_namespaces(sanic_client, user_headers) -> None:
 
 
 @pytest.mark.asyncio
+async def test_list_namespaces_all_groups_are_public(sanic_client, user_headers, member_1_headers) -> None:
+    payload = {
+        "name": "Group 1",
+        "slug": "group-1",
+    }
+    _, response = await sanic_client.post("/api/data/groups", headers=user_headers, json=payload)
+    assert response.status_code == 201, response.text
+    payload = {
+        "name": "Group 2",
+        "slug": "group-2",
+    }
+    _, response = await sanic_client.post("/api/data/groups", headers=member_1_headers, json=payload)
+    assert response.status_code == 201, response.text
+
+    _, response = await sanic_client.get("/api/data/namespaces", headers=user_headers)
+
+    assert response.status_code == 200, response.text
+    res_json = response.json
+    assert len(res_json) == 3
+    user_ns = res_json[0]
+    assert user_ns["slug"] == "user.doe"
+    group_1_ns = res_json[1]
+    assert group_1_ns["slug"] == "group-1"
+    group_2_ns = res_json[2]
+    assert group_2_ns["slug"] == "group-2"
+
+
+@pytest.mark.asyncio
+async def test_list_namespaces_filter_editor(sanic_client, user_headers, member_1_headers) -> None:
+    payload = {
+        "name": "Group 1",
+        "slug": "group-1",
+    }
+    _, response = await sanic_client.post("/api/data/groups", headers=user_headers, json=payload)
+    assert response.status_code == 201, response.text
+    payload = {
+        "name": "Group 2",
+        "slug": "group-2",
+    }
+    _, response = await sanic_client.post("/api/data/groups", headers=member_1_headers, json=payload)
+    assert response.status_code == 201, response.text
+    payload = {
+        "name": "Group 3",
+        "slug": "group-3",
+    }
+    _, response = await sanic_client.post("/api/data/groups", headers=member_1_headers, json=payload)
+    assert response.status_code == 201, response.text
+    new_members = [{"id": "user", "role": "editor"}]
+    _, response = await sanic_client.patch(
+        "/api/data/groups/group-3/members", headers=member_1_headers, json=new_members
+    )
+    assert response.status_code == 200, response.text
+
+    _, response = await sanic_client.get("/api/data/namespaces?minimum_role=editor", headers=user_headers)
+
+    assert response.status_code == 200, response.text
+    res_json = response.json
+    assert len(res_json) == 3
+    user_ns = res_json[0]
+    assert user_ns["slug"] == "user.doe"
+    group_1_ns = res_json[1]
+    assert group_1_ns["slug"] == "group-1"
+    group_2_ns = res_json[2]
+    assert group_2_ns["slug"] == "group-3"
+
+
+@pytest.mark.asyncio
 async def test_get_namespace_by_slug(sanic_client, user_headers) -> None:
     payload = {
         "name": "Group1",
