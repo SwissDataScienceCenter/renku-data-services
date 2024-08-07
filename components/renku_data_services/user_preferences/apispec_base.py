0a77cce78f443f7a09a7b015849a5bfab4c49a20
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2024-08-07 12:11:23 +0200

    test: fix schemathesis query parameters test (#340)

--- components/renku_data_services/user_preferences/apispec_base.py
+++ components/renku_data_services/user_preferences/apispec_base.py
@@ -1,6 +1,6 @@
 """Base models for API specifications."""
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 
 class BaseAPISpec(BaseModel):
@@ -10,14 +10,3 @@ class BaseAPISpec(BaseModel):
         """Enables orm mode for pydantic."""
 
         from_attributes = True
-
-
-class PinnedProjectFilter(BaseAPISpec):
-    """The schema for the query parameters used to filter pinned projects."""
-
-    class Config:
-        """Configuration."""
-
-        extra = "ignore"
-
-    project_slug: str = Field(default="")
