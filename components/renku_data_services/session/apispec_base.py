a6e8243b1c65233820609a29eab359334980297e
Tasko Olevski <olevski90@gmail.com>
2024-09-20 16:13:37 +0200

    feat!: expand environments specification (#338)
    This is a breaking change in the API. It requires changes in the UI for it to be fully useful.

--- components/renku_data_services/session/apispec_base.py
+++ components/renku_data_services/session/apispec_base.py
@@ -1,5 +1,6 @@
 """Base models for API specifications."""
 
+from pathlib import PurePosixPath
 from typing import Any
 
 from pydantic import BaseModel, field_validator
@@ -21,3 +22,11 @@ class BaseAPISpec(BaseModel):
         if isinstance(value, ULID):
             return str(value)
         return value
+
+    @field_validator("working_directory", "mount_directory", check_fields=False, mode="before")
+    @classmethod
+    def convert_path_to_string(cls, val: str | PurePosixPath) -> str:
+        """Converts the python path to a regular string when pydantic deserializes."""
+        if isinstance(val, PurePosixPath):
+            return val.as_posix()
+        return val
