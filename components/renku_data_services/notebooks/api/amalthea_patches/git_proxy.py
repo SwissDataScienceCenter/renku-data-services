9118c1e709e3d894b7e205eee507e03d2bab8c32
Tasko Olevski <olevski90@gmail.com>
2024-09-23 13:52:30 +0200

    feat: run notebooks in data service (#375)
    Co-authored-by: Samuel Gaist <samuel.gaist@idiap.ch>
    
    squashme: resolve package version conflicts

--- components/renku_data_services/notebooks/api/amalthea_patches/git_proxy.py
+++ components/renku_data_services/notebooks/api/amalthea_patches/git_proxy.py
@@ -4,18 +4,20 @@ import json
 from dataclasses import asdict
 from typing import TYPE_CHECKING, Any
 
+from kubernetes import client
+
 from renku_data_services.notebooks.api.amalthea_patches.utils import get_certificates_volume_mounts
-from renku_data_services.notebooks.api.classes.user import AnonymousUser
 
 if TYPE_CHECKING:
+    # NOTE: If these are directly imported then you get circular imports.
     from renku_data_services.notebooks.api.classes.server import UserServer
 
 
-def main(server: "UserServer") -> list[dict[str, Any]]:
+async def main_container(server: "UserServer") -> client.V1Container | None:
     """The patch that adds the git proxy container to a session statefulset."""
-    user_is_anonymous = isinstance(server.user, AnonymousUser)
-    if user_is_anonymous or not server.repositories:
-        return []
+    repositories = await server.repositories()
+    if not server.user.is_authenticated or not repositories:
+        return None
 
     etc_cert_volume_mount = get_certificates_volume_mounts(
         server.config,
@@ -23,39 +25,84 @@ def main(server: "UserServer") -> list[dict[str, Any]]:
         etc_certs=True,
         read_only_etc_certs=True,
     )
-    patches = []
 
     prefix = "GIT_PROXY_"
+    git_providers = await server.git_providers()
+    repositories = await server.repositories()
     env = [
-        {"name": f"{prefix}PORT", "value": str(server.config.sessions.git_proxy.port)},
-        {"name": f"{prefix}HEALTH_PORT", "value": str(server.config.sessions.git_proxy.health_port)},
-        {
-            "name": f"{prefix}ANONYMOUS_SESSION",
-            "value": "true" if user_is_anonymous else "false",
-        },
-        {"name": f"{prefix}RENKU_ACCESS_TOKEN", "value": str(server.user.access_token)},
-        {"name": f"{prefix}RENKU_REFRESH_TOKEN", "value": str(server.user.refresh_token)},
-        {"name": f"{prefix}RENKU_REALM", "value": server.config.keycloak_realm},
-        {
-            "name": f"{prefix}RENKU_CLIENT_ID",
-            "value": str(server.config.sessions.git_proxy.renku_client_id),
-        },
-        {
-            "name": f"{prefix}RENKU_CLIENT_SECRET",
-            "value": str(server.config.sessions.git_proxy.renku_client_secret),
-        },
-        {"name": f"{prefix}RENKU_URL", "value": "https://" + server.config.sessions.ingress.host},
-        {
-            "name": f"{prefix}REPOSITORIES",
-            "value": json.dumps([asdict(repo) for repo in server.repositories]),
-        },
-        {
-            "name": f"{prefix}PROVIDERS",
-            "value": json.dumps(
-                [dict(id=provider.id, access_token_url=provider.access_token_url) for provider in server.git_providers]
+        client.V1EnvVar(name=f"{prefix}PORT", value=str(server.config.sessions.git_proxy.port)),
+        client.V1EnvVar(name=f"{prefix}HEALTH_PORT", value=str(server.config.sessions.git_proxy.health_port)),
+        client.V1EnvVar(
+            name=f"{prefix}ANONYMOUS_SESSION",
+            value="false" if server.user.is_authenticated else "true",
+        ),
+        client.V1EnvVar(name=f"{prefix}RENKU_ACCESS_TOKEN", value=str(server.user.access_token)),
+        client.V1EnvVar(name=f"{prefix}RENKU_REFRESH_TOKEN", value=str(server.user.refresh_token)),
+        client.V1EnvVar(name=f"{prefix}RENKU_REALM", value=server.config.keycloak_realm),
+        client.V1EnvVar(
+            name=f"{prefix}RENKU_CLIENT_ID",
+            value=str(server.config.sessions.git_proxy.renku_client_id),
+        ),
+        client.V1EnvVar(
+            name=f"{prefix}RENKU_CLIENT_SECRET",
+            value=str(server.config.sessions.git_proxy.renku_client_secret),
+        ),
+        client.V1EnvVar(name=f"{prefix}RENKU_URL", value="https://" + server.config.sessions.ingress.host),
+        client.V1EnvVar(
+            name=f"{prefix}REPOSITORIES",
+            value=json.dumps([asdict(repo) for repo in repositories]),
+        ),
+        client.V1EnvVar(
+            name=f"{prefix}PROVIDERS",
+            value=json.dumps(
+                [dict(id=provider.id, access_token_url=provider.access_token_url) for provider in git_providers]
             ),
-        },
+        ),
     ]
+    container = client.V1Container(
+        image=server.config.sessions.git_proxy.image,
+        security_context={
+            "fsGroup": 100,
+            "runAsGroup": 1000,
+            "runAsUser": 1000,
+            "allowPrivilegeEscalation": False,
+            "runAsNonRoot": True,
+        },
+        name="git-proxy",
+        env=env,
+        liveness_probe={
+            "httpGet": {
+                "path": "/health",
+                "port": server.config.sessions.git_proxy.health_port,
+            },
+            "initialDelaySeconds": 3,
+        },
+        readiness_probe={
+            "httpGet": {
+                "path": "/health",
+                "port": server.config.sessions.git_proxy.health_port,
+            },
+            "initialDelaySeconds": 3,
+        },
+        volume_mounts=etc_cert_volume_mount,
+        resources={
+            "requests": {"memory": "16Mi", "cpu": "50m"},
+        },
+    )
+    return container
+
+
+async def main(server: "UserServer") -> list[dict[str, Any]]:
+    """The patch that adds the git proxy container to a session statefulset."""
+    repositories = await server.repositories()
+    if not server.user.is_authenticated or not repositories:
+        return []
+
+    container = await main_container(server)
+    if not container:
+        return []
+
+    patches = []
 
     patches.append(
         {
@@ -64,37 +111,8 @@ def main(server: "UserServer") -> list[dict[str, Any]]:
                 {
                     "op": "add",
                     "path": "/statefulset/spec/template/spec/containers/-",
-                    "value": {
-                        "image": server.config.sessions.git_proxy.image,
-                        "securityContext": {
-                            "fsGroup": 100,
-                            "runAsGroup": 1000,
-                            "runAsUser": 1000,
-                            "allowPrivilegeEscalation": False,
-                            "runAsNonRoot": True,
-                        },
-                        "name": "git-proxy",
-                        "env": env,
-                        "livenessProbe": {
-                            "httpGet": {
-                                "path": "/health",
-                                "port": server.config.sessions.git_proxy.health_port,
-                            },
-                            "initialDelaySeconds": 3,
-                        },
-                        "readinessProbe": {
-                            "httpGet": {
-                                "path": "/health",
-                                "port": server.config.sessions.git_proxy.health_port,
-                            },
-                            "initialDelaySeconds": 3,
-                        },
-                        "volumeMounts": etc_cert_volume_mount,
-                        "resources": {
-                            "requests": {"memory": "16Mi", "cpu": "50m"},
-                        },
-                    },
-                }
+                    "value": client.ApiClient().sanitize_for_serialization(container),
+                },
             ],
         }
     )
--- components/renku_data_services/notebooks/util/retries.py
+++ components/renku_data_services/notebooks/util/retries.py
@@ -1,9 +1,10 @@
 """Methods for retrying requests."""
 
+import asyncio
 import functools
-from collections.abc import Callable
+from collections.abc import Awaitable, Callable
 from time import sleep
-from typing import Any, Concatenate, ParamSpec, TypeVar
+from typing import Concatenate, ParamSpec, TypeVar
 
 from renku_data_services.notebooks.errors.intermittent import RetryTimeoutError
 
@@ -12,7 +13,7 @@ _Params = ParamSpec("_Params")
 
 
 def retry_with_exponential_backoff(
-    should_retry: Callable[[Any], bool],
+    should_retry: Callable[[_RetType], bool],
     num_retries: int = 10,
     initial_wait_ms: int = 20,
     multiplier: float = 2.0,
@@ -34,6 +35,41 @@ def retry_with_exponential_backoff(
                 if not should_retry(res):
                     return res
                 sleep(initial_wait_ms * (multiplier**i) / 1000)
+            raise RetryTimeoutError(f"Retrying the function {func.__name__} timed out after {num_retries} retries.")
+
+        return wrapper_retry
+
+    return decorator_retry
+
+
+def retry_with_exponential_backoff_async(
+    should_retry: Callable[[_RetType], bool],
+    num_retries: int = 10,
+    initial_wait_ms: int = 20,
+    multiplier: float = 2.0,
+) -> Callable[
+    [Callable[Concatenate[_Params], Awaitable[_RetType]]], Callable[Concatenate[_Params], Awaitable[_RetType]]
+]:
+    """Retries the wrapped function with an exponential backoff.
+
+    The should_retry "callback" is passed the results from calling the wrapped function.
+    If the response is true, the function is called again, otherwise the loop ends and
+    the result of the wrapped function is returned.
+
+    With the default values the wait times start with 20ms and then double every iteration.
+    """
+
+    def decorator_retry(
+        func: Callable[Concatenate[_Params], Awaitable[_RetType]],
+    ) -> Callable[Concatenate[_Params], Awaitable[_RetType]]:
+        @functools.wraps(func)
+        async def wrapper_retry(*args: _Params.args, **kwargs: _Params.kwargs) -> _RetType:
+            for i in range(num_retries):
+                res = await func(*args, **kwargs)
+                if not should_retry(res):
+                    return res
+
+                await asyncio.sleep(initial_wait_ms * (multiplier**i) / 1000)
             raise RetryTimeoutError(f"Retrying the function {func.__name__} timed out after {num_retries} retries.")
 
         return wrapper_retry
