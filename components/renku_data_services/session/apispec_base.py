308e6a31ed329b926178ed5826bedb3785b3ba9b
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2024-10-07 11:41:31 +0200

    refactor: add validation to project, storage, repo and session blueprints (#347)

--- components/renku_data_services/session/apispec_base.py
+++ components/renku_data_services/session/apispec_base.py
@@ -6,6 +6,8 @@ from typing import Any
 from pydantic import BaseModel, field_validator
 from ulid import ULID
 
+from renku_data_services.session import models
+
 
 class BaseAPISpec(BaseModel):
     """Base API specification."""
@@ -22,6 +24,28 @@ class BaseAPISpec(BaseModel):
         if isinstance(value, ULID):
             return str(value)
         return value
+
+    @field_validator("project_id", mode="before", check_fields=False)
+    @classmethod
+    def serialize_project_id(cls, project_id: str | ULID) -> str:
+        """Custom serializer that can handle ULIDs."""
+        return str(project_id)
+
+    @field_validator("environment_id", mode="before", check_fields=False)
+    @classmethod
+    def serialize_environment_id(cls, environment_id: str | ULID | None) -> str | None:
+        """Custom serializer that can handle ULIDs."""
+        if environment_id is None:
+            return None
+        return str(environment_id)
+
+    @field_validator("environment_kind", mode="before", check_fields=False)
+    @classmethod
+    def serialize_environment_kind(cls, environment_kind: models.EnvironmentKind | str) -> str:
+        """Custom serializer that can handle ULIDs."""
+        if isinstance(environment_kind, models.EnvironmentKind):
+            return environment_kind.value
+        return environment_kind
 
     @field_validator("working_directory", "mount_directory", check_fields=False, mode="before")
     @classmethod
