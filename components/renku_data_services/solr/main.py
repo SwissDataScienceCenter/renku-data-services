bbd05d6259ce7ab488856dd31dca7c15cf99666d
Eike <eikek@posteo.de>
2025-01-28 11:57:18 +0100

    switch to pydantic

--- components/renku_data_services/solr/main.py
+++ components/renku_data_services/solr/main.py
@@ -3,37 +3,55 @@
 import asyncio
 import logging
 
-from renku_data_services.solr import solr_client, solr_schema
+from renku_data_services.solr.solr_schema import (
+    Analyzer,
+    Field,
+    FieldName,
+    FieldType,
+    TypeName,
+    CopyFieldRule,
+    AddCommand,
+    SchemaCommandList,
+    FieldTypeClasses,
+    Filters,
+    Tokenizer,
+)
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 
 
+# async def _test():
+#     client = await solr_client.DefaultSolrClient("http://rsdevcnt:8983/solr/renku-search-dev")
+#     doc = await client.query("*:*")
+#     await client.close()
+#     print(doc.docs)
+
+
 async def _test():
-    client = await solr_client.DefaultSolrClient("http://rsdevcnt:8983/solr/renku-search-dev")
-    doc = await client.query("*:*")
-    await client.close()
-    print(doc.docs)
+    tokenizer = Tokenizer(name=TypeName("classic"))
+    filter = Filters.edgeNgram()
+    analyzer = Analyzer(tokenizer=tokenizer, filters=[filter, Filters.ascii_folding])
+    ftype = FieldType(name=TypeName("project_name_type"), clazz=FieldTypeClasses.type_str, indexed=True)
+    field = Field(name=FieldName("project_name"), type=ftype.name, indexed=True)
+    print(field.to_json())
 
 
 async def _test2():
-    tokenizer = solr_schema.Tokenizer(solr_schema.TypeName("classic"))
-    filter = solr_schema.Filters.NGRAM
-    analyzer = solr_schema.Analyzer(tokenizer, [filter])
-    ft = solr_schema.FieldType(
-        name=solr_schema.TypeName("name_s"), clazz=solr_schema.FieldTypeClasses.TYPE_TEXT, index_analyzer=analyzer
-    )
-    field = solr_schema.Field.of(name=solr_schema.FieldName("project_name"), type=ft)
+    tokenizer = Tokenizer(name=TypeName("classic"))
+    filter = Filters.ngram
+    analyzer = Analyzer(tokenizer=tokenizer, filters=[filter])
+    ft = FieldType(name=TypeName("name_s"), clazz=FieldTypeClasses.type_text, index_analyzer=analyzer)
+    field = Field.of(name=FieldName("project_name"), type=ft)
     field.required = True
     field.indexed = True
     field.multiValued = True
-    cmd = solr_schema.AddCommand(field)
-    cmds = solr_schema.SchemaCommandList(
+    cmds = SchemaCommandList(
         [
-            cmd,
-            solr_schema.AddCommand(
-                solr_schema.CopyFieldRule(solr_schema.FieldName("other_name_s"), solr_schema.FieldName("target_name_s"))
-            ),
+            AddCommand(ft),
+            AddCommand(CopyFieldRule(source=FieldName("other_name_s"), dest=FieldName("target_name_s"))),
+            AddCommand(field),
+            AddCommand(Field.of(name=FieldName("user_name"), type=ft)),
         ]
     )
     print(cmds.to_json())
--- components/renku_data_services/solr/solr_schema.py
+++ components/renku_data_services/solr/solr_schema.py
@@ -1,55 +1,60 @@
 """Schema modification for solr."""
 
 from abc import abstractmethod
-from dataclasses import dataclass, field
-from dataclasses_json import dataclass_json, config
+from dataclasses import dataclass
+from pydantic import BaseModel, model_serializer
 from typing import NewType, final, Self, Any
 import json
 
+import pydantic
 
-def _excludeWhen(v):
-    return lambda x: x == v
 
 
 TypeName = NewType("TypeName", str)
 FieldName = NewType("FieldName", str)
 
 
-@dataclass_json
-@dataclass
+class SchemaModel(BaseModel):
+    def to_dict(self) -> dict[str, Any]:
+        return self.model_dump(by_alias=True, exclude_defaults=True)
+
+    def to_json(self) -> str:
+        return json.dumps(self.to_dict())
+
 @final
-class Tokenizer:
+class Tokenizer(SchemaModel):
     name: TypeName
 
 
-
-@dataclass
 @final
-class Filter:
+class Filter(BaseModel):
     """Defines a SOLR filter. See https://solr.apache.org/guide/solr/latest/indexing-guide/filters.html."""
 
     name: str
-    settings: dict | None = field(metadata=config(exclude=lambda v: v is None), default=None)
+    settings: dict | None = None
 
-    def to_json(self) -> str:
+    @model_serializer()
+    def to_dict(self) -> dict[str, Any]:
         match self.settings:
             case None:
-                return json.dumps({"name":self.name})
+                return {"name":self.name}
             case _:
                 data = self.settings.copy()
                 data.update({"name": self.name})
-                return json.dumps(data)
+                return data
+
+
 
 @final
 class Filters:
     """A list of predefined filters supported by SOLR."""
 
-    ASCII_FOLDING = Filter("asciiFolding")
-    LOWERCASE = Filter("lowercase")
-    STOP = Filter("stop")
-    ENGLISH_MINIMAL_STEM = Filter("englishMinimalStem")
-    CLASSIC = Filter("classic")
-    NGRAM = Filter("nGram")
+    ascii_folding = Filter(name = "asciiFolding")
+    lowercase = Filter(name = "lowercase")
+    stop = Filter(name = "stop")
+    english_minimal_stem = Filter(name="englishMinimalStem")
+    classic = Filter(name="classic")
+    ngram = Filter(name="nGram")
 
     @classmethod
     def edgeNgram(cls, min_gram_size: int = 3, maxGramSize: int = 6, preserve_original: bool = True) -> Filter:
@@ -64,119 +69,108 @@ class Filters:
         )
 
 
-@dataclass_json
-@dataclass
 @final
-class Analyzer:
+class Analyzer(SchemaModel):
     tokenizer: Tokenizer
-    filters: list[Filter] = field(default_factory=list)
+    filters: list[Filter] = pydantic.Field(default_factory=list)
 
 
 FieldTypeClass = NewType("FieldTypeClass", str)
 
 
-# look if enum makes sense
 class FieldTypeClasses:
     """A list of field type classses."""
 
     type_int = FieldTypeClass("IntPointField")
-    TYPE_LONG = FieldTypeClass("LongPointField")
-    TYPE_FLOAT = FieldTypeClass("FloatPointField")
-    TYPE_DOUBLE = FieldTypeClass("DoublePointField")
-    TYPE_TEXT = FieldTypeClass("TextField")
-    TYPE_STR = FieldTypeClass("StrField")
-    TYPE_UUID = FieldTypeClass("UUIDField")
-    TYPE_RANK = FieldTypeClass("RankField")
-    TYPE_DATE_POINT = FieldTypeClass("DatePointField")
-    TYPE_DATE_RANGE = FieldTypeClass("DateRangeField")
-    TYPE_BOOL = FieldTypeClass("BoolField")
+    type_long = FieldTypeClass("LongPointField")
+    type_float = FieldTypeClass("FloatPointField")
+    type_double = FieldTypeClass("DoublePointField")
+    type_text = FieldTypeClass("TextField")
+    type_str = FieldTypeClass("StrField")
+    type_uuid = FieldTypeClass("UUIDField")
+    type_rank = FieldTypeClass("RankField")
+    type_date_point = FieldTypeClass("DatePointField")
+    type_date_range = FieldTypeClass("DateRangeField")
+    type_bool = FieldTypeClass("BoolField")
 
 
-@dataclass_json
-@dataclass
 @final
-class FieldType:
+class FieldType(SchemaModel):
     name: TypeName
-    clazz: FieldTypeClass = field(metadata=config(field_name="class"))
-    index_analyzer: Analyzer | None = field(metadata=config(exclude=_excludeWhen(None)), default=None)
-    query_analyzer: Analyzer | None = field(metadata=config(exclude=_excludeWhen(None)), default=None)
-    required: bool = field(metadata=config(exclude=_excludeWhen(False)), default=False)
-    indexed: bool = field(metadata=config(exclude=_excludeWhen(False)), default=False)
-    stored: bool = field(metadata=config(exclude=_excludeWhen(True)), default=True)
-    multi_valued: bool = field(metadata=config(exclude=_excludeWhen(False)), default=False)
-    uninvertible: bool = field(metadata=config(exclude=_excludeWhen(False)), default=False)
-    doc_values: bool = field(metadata=config(exclude=_excludeWhen(False)), default=False)
-    sort_missing_last: bool = field(metadata=config(exclude=_excludeWhen(True)), default=True)
+    clazz: FieldTypeClass = pydantic.Field(serialization_alias="class")
+    index_analyzer: Analyzer | None = None
+    query_analyzer: Analyzer | None = None
+    required: bool = False
+    indexed: bool = False
+    stored: bool = True
+    multi_valued: bool = False
+    uninvertible: bool = False
+    doc_values: bool = False
+    sort_missing_last: bool = True
 
     @classmethod
     def id(cls, name: TypeName) -> Self:
         """Create a field that can be used as a document id."""
-        return FieldType(name, FieldTypeClasses.TYPE_STR)
+        return FieldType(name=name, clazz=FieldTypeClasses.type_str)
 
     @classmethod
     def text(cls, name: TypeName) -> Self:
-        return FieldType(name, FieldTypeClasses.TYPE_TEXT)
+        return FieldType(name=name, clazz=FieldTypeClasses.type_text)
 
     @classmethod
     def str(cls, name: TypeName) -> Self:
-        return FieldType(name, FieldTypeClasses.TYPE_STR)
+        return FieldType(name=name, clazz=FieldTypeClasses.type_str)
 
     @classmethod
     def int(cls, name: TypeName) -> Self:
-        return FieldType(name, FieldTypeClasses.TYPE_INT)
+        return FieldType(name=name, clazz=FieldTypeClasses.type_int)
 
     @classmethod
     def long(cls, name: TypeName) -> Self:
-        return FieldType(name, FieldTypeClasses.TYPE_LONG)
+        return FieldType(name=name, clazz=FieldTypeClasses.type_long)
 
     @classmethod
     def double(cls, name: TypeName) -> Self:
-        return FieldType(name, FieldTypeClasses.TYPE_DOUBLE)
+        return FieldType(name=name, clazz=FieldTypeClasses.type_double)
 
     @classmethod
     def dateTime(cls, name: TypeName) -> Self:
-        return FieldType(name, FieldTypeClasses.TYPE_DATE_RANGE)
+        return FieldType(name=name, clazz=FieldTypeClasses.type_date_range)
 
 
-@dataclass_json
-@dataclass
 @final
-class Field:
+class Field(SchemaModel):
     name: FieldName
     type: TypeName
-    required: bool = field(metadata=config(exclude=_excludeWhen(False)), default=False)
-    indexed: bool = field(metadata=config(exclude=_excludeWhen(True)), default=True)
-    stored: bool = field(metadata=config(exclude=_excludeWhen(True)), default=True)
-    multiValued: bool = field(metadata=config(exclude=_excludeWhen(False)), default=False)
-    uninvertible: bool = field(metadata=config(exclude=_excludeWhen(True)), default=True)
-    docValues: bool = field(metadata=config(exclude=_excludeWhen(False)), default=False)
+    required: bool = False
+    indexed: bool = True
+    stored: bool = True
+    multiValued: bool = False
+    uninvertible: bool = True
+    docValues: bool = False
 
     @classmethod
     def of(cls, name: FieldName, type: FieldType) -> Self:
         return Field(name=name, type=type.name)
 
 
-@dataclass_json
-@dataclass
 @final
-class DynamicFieldRule:
+class DynamicFieldRule(SchemaModel):
     name: FieldName
     type: TypeName
-    required: bool = field(metadata=config(exclude=_excludeWhen(False)), default=False)
-    indexed: bool = field(metadata=config(exclude=_excludeWhen(True)), default=True)
-    stored: bool = field(metadata=config(exclude=_excludeWhen(True)), default=True)
-    multiValued: bool = field(metadata=config(exclude=_excludeWhen(False)), default=False)
-    uninvertible: bool = field(metadata=config(exclude=_excludeWhen(False)), default=False)
-    docValues: bool = field(metadata=config(exclude=_excludeWhen(False)), default=False)
+    required: bool = False
+    indexed: bool = True
+    stored: bool = True
+    multiValued: bool = False
+    uninvertible: bool = False
+    docValues: bool = False
 
 
-@dataclass_json
-@dataclass
 @final
-class CopyFieldRule:
+class CopyFieldRule(SchemaModel):
     source: FieldName
     dest: FieldName
-    maxChars: int | None = field(metadata=config(exclude=_excludeWhen(None)), default=None)
+    maxChars: int | None = None
 
 
 class SchemaCommand:
@@ -189,35 +183,35 @@ class SchemaCommand:
 
 @dataclass
 @final
-class AddCommand(SchemaCommand):
+class AddCommand:
     value: Field | FieldType | DynamicFieldRule | CopyFieldRule
 
     def command_name(self) -> str:
         match self.value:
-            case Field(_):
+            case Field():
                 return "add-field"
 
-            case FieldType(_):
+            case FieldType():
                 return "add-field-type"
 
-            case DynamicFieldRule(_):
+            case DynamicFieldRule():
                 return "add-dynamic-field"
 
-            case CopyFieldRule(_):
+            case CopyFieldRule():
                 return "add-copy-field"
 
     def to_dict(self) -> dict[str, Any]:
         match self.value:
-            case Field(_) as f:
+            case Field() as f:
                 return f.to_dict()
 
-            case FieldType(_) as f:
+            case FieldType() as f:
                 return f.to_dict()
 
-            case DynamicFieldRule(_) as f:
+            case DynamicFieldRule() as f:
                 return f.to_dict()
 
-            case CopyFieldRule(_) as f:
+            case CopyFieldRule() as f:
                 return f.to_dict()
 
 
