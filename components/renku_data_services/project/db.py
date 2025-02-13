924d2054b4c15f309cca86a8a1778c895d9bef82
Andrea Cordoba <dandrea.cordoba@gmail.com>
2025-02-13 09:52:03 +0100

    no reuse session to check error

--- components/renku_data_services/project/db.py
+++ components/renku_data_services/project/db.py
@@ -931,7 +931,7 @@ class ProjectMigrationRepository:
         project_migration = result.one_or_none()
         if project_migration is not None:
             raise errors.ValidationError(message=f"Project V1 with id '{project_v1_id}' already exists.")
-        created_project = await self.project_repo.insert_project(user, project, session=session)
+        created_project = await self.project_repo.insert_project(user, project)
         if not created_project:
             raise errors.ValidationError(
                 message=f"Failed to create a project for migration from v1 (project_v1_id={project_v1_id})."
