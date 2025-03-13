e1b03e7dab8c8d871c338c3510c18a9e27321855
Lionel Sambuc <lionel.sambuc@epfl.ch>
2025-03-13 16:24:54 +0100

    fix: Update apispec_base
    * Among other things this version supports ULID serialization /
      deserialization

--- components/renku_data_services/crc/apispec_base.py
+++ components/renku_data_services/crc/apispec_base.py
@@ -1,6 +1,12 @@
 """Base models for API specifications."""
 
-from pydantic import BaseModel
+from pathlib import PurePosixPath
+from typing import Any
+
+from pydantic import BaseModel, field_validator
+from ulid import ULID
+
+from renku_data_services.session import models
 
 
 class BaseAPISpec(BaseModel):
@@ -10,3 +16,41 @@ class BaseAPISpec(BaseModel):
         """Enables orm mode for pydantic."""
 
         from_attributes = True
+
+    @field_validator("*", mode="before", check_fields=False)
+    @classmethod
+    def serialize_ulid(cls, value: Any) -> Any:
+        """Handle ULIDs."""
+        if isinstance(value, ULID):
+            return str(value)
+        return value
+
+    @field_validator("project_id", mode="before", check_fields=False)
+    @classmethod
+    def serialize_project_id(cls, project_id: str | ULID) -> str:
+        """Custom serializer that can handle ULIDs."""
+        return str(project_id)
+
+    @field_validator("environment_id", mode="before", check_fields=False)
+    @classmethod
+    def serialize_environment_id(cls, environment_id: str | ULID | None) -> str | None:
+        """Custom serializer that can handle ULIDs."""
+        if environment_id is None:
+            return None
+        return str(environment_id)
+
+    @field_validator("environment_kind", mode="before", check_fields=False)
+    @classmethod
+    def serialize_environment_kind(cls, environment_kind: models.EnvironmentKind | str) -> str:
+        """Custom serializer that can handle ULIDs."""
+        if isinstance(environment_kind, models.EnvironmentKind):
+            return environment_kind.value
+        return environment_kind
+
+    @field_validator("working_directory", "mount_directory", check_fields=False, mode="before")
+    @classmethod
+    def convert_path_to_string(cls, val: str | PurePosixPath) -> str:
+        """Converts the python path to a regular string when pydantic deserializes."""
+        if isinstance(val, PurePosixPath):
+            return val.as_posix()
+        return val
