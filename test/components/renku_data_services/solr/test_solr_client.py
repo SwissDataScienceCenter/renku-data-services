e07ffc7fa91a2d7d06c2a1fb8a3856822db97c86
Eike Kettner <eike.kettner@sdsc.ethz.ch>
2025-03-13 11:55:21 +0100

    Add facet requests and response to solr client

--- test/components/renku_data_services/solr/test_solr_client.py
+++ test/components/renku_data_services/solr/test_solr_client.py
@@ -1,5 +1,6 @@
 import random
 import string
+import json
 
 import pytest
 
@@ -12,6 +13,13 @@ from renku_data_services.solr.solr_client import (
     DocVersions,
     SolrClientConfig,
     SolrClientCreateCoreException,
+    FacetArbitraryRange,
+    FacetBuckets,
+    FacetCount,
+    FacetRange,
+    FacetTerms,
+    SolrBucketFacetResponse,
+    SolrFacets,
     SolrQuery,
     SortDirection,
     SubQuery,
@@ -38,17 +46,161 @@ def test_serialize_document():
     assert nd == d
 
 
+def test_facet_terms() -> None:
+    ft = FacetTerms(name=FieldName("types"), field=Fields.entity_type)
+    assert ft.to_dict() == {
+        "types": {
+            "type": "terms",
+            "field": "_type",
+            "missing": False,
+            "numBuckets": False,
+            "allBuckets": False,
+        }
+    }
+    ft = FacetTerms(name=FieldName("cat"), field=FieldName("category"), limit=100)
+    assert ft.to_dict() == {
+        "cat": {
+            "type": "terms",
+            "field": "category",
+            "limit": 100,
+            "missing": False,
+            "numBuckets": False,
+            "allBuckets": False,
+        }
+    }
+
+
+def test_facet_range() -> None:
+    fr = FacetArbitraryRange(
+        name=FieldName("stars"),
+        field=FieldName("stars"),
+        ranges=[FacetRange(start="*", to=100), FacetRange(start=100, to=200), FacetRange(start=200, to="*")],
+    )
+    assert fr.to_dict() == {
+        "stars": {
+            "type": "range",
+            "field": "stars",
+            "ranges": [{"from": "*", "to": 100}, {"from": 100, "to": 200}, {"from": 200, "to": "*"}],
+        }
+    }
+
+
+def test_solr_facets() -> None:
+    fc = SolrFacets.of(
+        FacetTerms(name=FieldName("types"), field=Fields.entity_type),
+        FacetArbitraryRange(
+            name=FieldName("stars"),
+            field=FieldName("stars"),
+            ranges=[FacetRange(start="*", to=100), FacetRange(start=100, to=200), FacetRange(start=200, to="*")],
+        ),
+    )
+    assert fc.to_dict() == {
+        "stars": {
+            "type": "range",
+            "field": "stars",
+            "ranges": [{"from": "*", "to": 100}, {"from": 100, "to": 200}, {"from": 200, "to": "*"}],
+        },
+        "types": {
+            "type": "terms",
+            "field": "_type",
+            "missing": False,
+            "numBuckets": False,
+            "allBuckets": False,
+        },
+    }
+
+
+def test_facet_buckets() -> None:
+    fb = FacetBuckets(
+        buckets=[FacetCount(field=FieldName("electronic"), count=5), FacetCount(field=FieldName("garden"), count=10)]
+    )
+    assert fb.to_dict() == {"buckets": [{"val": "electronic", "count": 5}, {"val": "garden", "count": 10}]}
+
+    fb_str = """{
+      "buckets":[
+         {"val":"electronics", "count":12},
+         {"val":"currency", "count":4},
+         {"val":"memory", "count":3}
+      ]
+    }"""
+    assert FacetBuckets.model_validate_json(fb_str) == FacetBuckets(
+        buckets=[
+            FacetCount(field=FieldName("electronics"), count=12),
+            FacetCount(field=FieldName("currency"), count=4),
+            FacetCount(field=FieldName("memory"), count=3),
+        ]
+    )
+
+
 def test_serialize_solr_query():
-    q1 = SolrQuery.query_all_fields("name:hello")
-    assert q1.to_dict() == {"query": "name:hello", "fields": ["*", "score"], "sort": ""}
+    q = SolrQuery.query_all_fields("name:hello")
+    assert q.to_dict() == {"query": "name:hello", "fields": ["*", "score"], "sort": ""}
 
-    q2 = SolrQuery.query_all_fields("name:hello").with_sort([(Fields.name, SortDirection.asc)])
-    assert q2.to_dict() == {"query": "name:hello", "fields": ["*", "score"], "sort": "name asc"}
+    q = SolrQuery.query_all_fields("name:hello").with_sort([(Fields.name, SortDirection.asc)])
+    assert q.to_dict() == {"query": "name:hello", "fields": ["*", "score"], "sort": "name asc"}
 
-    q3 = SolrQuery.query_all_fields("name:hello").with_sort(
+    q = SolrQuery.query_all_fields("name:hello").with_sort(
         [(Fields.name, SortDirection.asc), (Fields.creation_date, SortDirection.desc)]
     )
-    assert q3.to_dict() == {"query": "name:hello", "fields": ["*", "score"], "sort": "name asc,creationDate desc"}
+    assert q.to_dict() == {"query": "name:hello", "fields": ["*", "score"], "sort": "name asc,creationDate desc"}
+
+    q = (
+        SolrQuery.query_all_fields("name:test help")
+        .with_facet(FacetTerms(name=FieldName("type"), field=FieldName("_type")))
+        .add_sub_query(FieldName("details"), SubQuery(query="test", filter="", limit=1))
+        .with_sort([(Fields.name, SortDirection.asc)])
+    )
+    assert q.to_dict() == {
+        "query": "name:test help",
+        "fields": ["*", "score", "details:[subquery]"],
+        "sort": "name asc",
+        "params": {"details.q": "test", "details.limit": "1"},
+        "facet": {
+            "type": {"type": "terms", "field": "_type", "missing": False, "numBuckets": False, "allBuckets": False}
+        },
+    }
+
+
+def test_solr_bucket_facet_response() -> None:
+    respones_str = """{
+      "count":32,
+      "categories":{
+        "buckets":[
+           {"val":"electronics", "count":12},
+           {"val":"currency", "count":4},
+           {"val":"memory", "count":3}
+        ]
+      },
+      "memories":{
+        "buckets":[
+           {"val":"bike", "count":2},
+           {"val":"chair", "count":4},
+           {"val":"memory", "count":6}
+        ]
+      }
+    }"""
+    fr = SolrBucketFacetResponse.model_validate_json(respones_str)
+    expected = SolrBucketFacetResponse(
+        count=32,
+        buckets={
+            FieldName("categories"): FacetBuckets(
+                buckets=[
+                    FacetCount(field=FieldName("electronics"), count=12),
+                    FacetCount(field=FieldName("currency"), count=4),
+                    FacetCount(field=FieldName("memory"), count=3),
+                ]
+            ),
+            FieldName("memories"): FacetBuckets(
+                buckets=[
+                    FacetCount(field=FieldName("bike"), count=2),
+                    FacetCount(field=FieldName("chair"), count=4),
+                    FacetCount(field=FieldName("memory"), count=6),
+                ]
+            ),
+        },
+    )
+    assert fr == expected
+    assert expected.to_dict() == json.loads(respones_str)
 
 
 @pytest.mark.asyncio
@@ -190,3 +342,28 @@ async def test_sub_query(solr_search):
         user = User.model_validate(user_doc)
         assert user.namespace == u2.namespace
         assert user.id == u2.id
+
+
+@pytest.mark.asyncio
+async def test_run_facet_query(solr_search):
+    async with DefaultSolrClient(solr_search) as client:
+        u1 = test_entity_documents.user_tadej_pogacar
+        u2 = test_entity_documents.user_jan_ullrich
+        p = test_entity_documents.project_ai_stuff
+        r1 = await client.upsert([u1, u2, p])
+        assert_upsert_result(r1)
+
+        query = SolrQuery.query_all_fields("_type:*").with_facet(
+            FacetTerms(name=Fields.entity_type, field=Fields.entity_type)
+        )
+
+        r2 = await client.query(query)
+        assert len(r2.response.docs) == 3
+        assert r2.facets == SolrBucketFacetResponse(
+            count=3,
+            buckets={
+                Fields.entity_type: FacetBuckets.of(
+                    FacetCount(field=FieldName("User"), count=2), FacetCount(field=FieldName("Project"), count=1)
+                )
+            },
+        )
