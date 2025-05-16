d1b86a9d16cd5953026895260a2a1a34b1cc13a4
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2025-05-16 13:00:34 +0200

    fix-tests

--- bases/renku_data_services/background_jobs/config.py
+++ bases/renku_data_services/background_jobs/config.py
@@ -1,5 +1,6 @@
 """Configurations for background jobs."""
 
+import os
 from dataclasses import dataclass
 
 from renku_data_services.app_config.config import KeycloakConfig
@@ -12,17 +13,21 @@ from renku_data_services.message_queue.config import RedisConfig
 class SyncConfig:
     """Main configuration."""
 
+    dummy_stores: bool
+
     authz: AuthzConfig
     redis: RedisConfig
     db: DBConfig
-    keycloak: KeycloakConfig
+    keycloak: KeycloakConfig | None
 
     @classmethod
     def from_env(cls) -> "SyncConfig":
         """Generate a configuration from environment variables."""
+        dummy_stores = os.environ.get("DUMMY_STORES", "false").lower() == "true"
         redis = RedisConfig.from_env()
         db = DBConfig.from_env()
 
         authz = AuthzConfig.from_env()
-        keycloak = KeycloakConfig.from_env()
-        return cls(authz=authz, redis=redis, db=db, keycloak=keycloak)
+
+        keycloak = None if dummy_stores else KeycloakConfig.from_env()
+        return cls(authz=authz, redis=redis, db=db, keycloak=keycloak, dummy_stores=dummy_stores)
