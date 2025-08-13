0f244e7cff023ad4592bfc8dd9e78ba96d499d46
Tasko Olevski <olevski90@gmail.com>
2025-08-14 00:31:10 +0200

    fix: set default cluster name back to renkulab

--- components/renku_data_services/k8s/constants.py
+++ components/renku_data_services/k8s/constants.py
@@ -7,7 +7,7 @@ from typing import Final, NewType
 # LSA Not enough time: Adapt this to be an alias to ULID
 ClusterId = NewType("ClusterId", str)
 
-DEFAULT_K8S_CLUSTER: Final[ClusterId] = ClusterId("0RENK1RENK2RENK3RENK4RENK5")  # This has to be a valid ULID
+DEFAULT_K8S_CLUSTER: Final[ClusterId] = ClusterId("renkulab")
 
 DUMMY_TASK_RUN_USER_ID: Final[str] = "DummyTaskRunUser"
 """The user id to use for TaskRuns in the k8s cache.
