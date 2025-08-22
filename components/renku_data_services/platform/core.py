3a8b2964f692816b93c44d3cd0fb9bf4173d3fd3
Chandrasekhar Ramakrishnan <ciyer@users.noreply.github.com>
2025-08-22 12:36:31 +0000

    feat: support managing redirects

--- components/renku_data_services/platform/core.py
+++ components/renku_data_services/platform/core.py
@@ -6,3 +6,11 @@ from renku_data_services.platform import apispec, models
 def validate_platform_config_patch(patch: apispec.PlatformConfigPatch) -> models.PlatformConfigPatch:
     """Validate the update to the platform configuration."""
     return models.PlatformConfigPatch(incident_banner=patch.incident_banner)
+
+
+def validate_url_redirect_post(post: apispec.UrlRedirectPlanPost) -> models.UnsavedUrlRedirectConfig:
+    """Validate the creation of a URL redirect."""
+    return models.UnsavedUrlRedirectConfig(
+        source_url=post.source_url,
+        target_url=post.target_url,
+    )
--- components/renku_data_services/platform/models.py
+++ components/renku_data_services/platform/models.py
@@ -4,6 +4,8 @@ from dataclasses import dataclass, field
 from datetime import UTC, datetime
 from enum import StrEnum
 
+from ulid import ULID
+
 from renku_data_services.utils.etag import compute_etag_from_timestamp
 
 
@@ -33,3 +35,25 @@ class PlatformConfigPatch:
     """Model for changes requested on the platform configuration."""
 
     incident_banner: str | None = None
+
+
+@dataclass(frozen=True, eq=True, kw_only=True)
+class UnsavedUrlRedirectConfig:
+    """Model representing a URL redirect that has not been persisted."""
+
+    source_url: str
+    target_url: str
+
+
+@dataclass(frozen=True, eq=True, kw_only=True)
+class UrlRedirectConfig(UnsavedUrlRedirectConfig):
+    """Model representing a redirect from a source URL to a target."""
+
+    id: ULID
+    creation_date: datetime = field(default_factory=lambda: datetime.now(UTC).replace(microsecond=0))
+    updated_at: datetime
+
+    @property
+    def etag(self) -> str:
+        """Entity tag value for this redirect object."""
+        return compute_etag_from_timestamp(self.updated_at)
