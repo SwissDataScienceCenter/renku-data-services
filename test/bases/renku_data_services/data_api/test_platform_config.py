127b1dfee2c1c472786bb606f9d2d6666ab4ca28
Chandrasekhar Ramakrishnan <ciyer@users.noreply.github.com>
2025-08-28 12:38:00 +0000

    wip

--- test/bases/renku_data_services/data_api/test_platform_config.py
+++ test/bases/renku_data_services/data_api/test_platform_config.py
@@ -48,3 +48,52 @@ async def test_patch_platform_config_unauthorized(
     _, res = await sanic_client.patch("/api/data/platform/config", headers=headers, json=payload)
 
     assert res.status_code == 403, res.text
+
+
+@pytest.mark.asyncio
+async def test_get_redirects(app_manager: DependencyManager, sanic_client: SanicASGITestClient) -> None:
+    parameters = {"page": 1, "per_page": 5}
+    _, res = await sanic_client.get("/api/data/platform/redirects", params=parameters)
+
+    assert res.status_code == 200, res.text
+    assert res.json is not None
+    assert res.headers["page"] == "1"
+    assert res.headers["per-page"] == "5"
+    assert res.headers["total"] == "0"
+    assert res.headers["total-pages"] == "1"
+
+    # redirects = res.json
+    # assert redirects.get("etag") != ""
+
+
+@pytest.mark.asyncio
+async def test_post_platform_config(sanic_client: SanicASGITestClient, admin_headers: dict[str, str]) -> None:
+    _, res = await sanic_client.get("/api/data/platform/redirects")
+    initial_config = res.json
+
+    headers = merge_headers(admin_headers, {"If-Match": initial_config["etag"]})
+    payload = {"source_url": "/projects/ns/project-slug", "target_url": "/p/ns/project-slug"}
+
+    _, res = await sanic_client.post("/api/data/platform/redirects", headers=headers, json=payload)
+    assert res.status_code == 500, res.text
+
+    # assert res.status_code == 201, res.text
+    # assert res.json is not None
+    # platform_config = res.json
+    # assert platform_config.get("incident_banner") == "Some content"
+    # assert platform_config.get("etag") != initial_config["etag"]
+
+
+@pytest.mark.asyncio
+async def test_post_platform_config_unauthorized(
+    sanic_client: SanicASGITestClient, user_headers: dict[str, str]
+) -> None:
+    _, res = await sanic_client.get("/api/data/platform/redirects")
+    initial_config = res.json
+
+    headers = merge_headers(user_headers, {"If-Match": initial_config["etag"]})
+    payload = {"source_url": "/projects/ns/project-slug", "target_url": "/p/ns/project-slug"}
+
+    _, res = await sanic_client.post("/api/data/platform/redirects", headers=headers, json=payload)
+
+    assert res.status_code == 403, res.text
