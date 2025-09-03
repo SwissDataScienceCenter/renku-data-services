96a4c25fa17e5f0911f0a694ed403582dd21d7a7
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2025-09-03 11:42:25 +0000

    fix migration

--- components/renku_data_services/migrations/versions/f6705f482551_add_cscs.py
+++ components/renku_data_services/migrations/versions/f6705f482551_add_cscs.py
@@ -6,7 +6,7 @@ Create Date: 2025-06-17 14:09:43.322460
 
 """
 
-# from alembic import op
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "f6705f482551"
@@ -16,10 +16,15 @@ depends_on = None
 
 
 def upgrade() -> None:
-    # op.execute("ALTER TYPE providerkind ADD VALUE 'cscs'")
-    pass
+    op.execute("ALTER TYPE providerkind ADD VALUE 'cscs'")
 
 
 def downgrade() -> None:
     # NOTE: Postgres does not allow removing values from an enum
-    pass
+    op.execute("DELETE FROM connected_services.oauth2_clients WHERE kind = 'cscs'")
+    op.execute("ALTER TYPE providerkind RENAME TO providerkind_old;")
+    op.execute("CREATE TYPE providerkind AS ENUM ('gitlab', 'github', 'drive', 'onedrive', 'dropbox')")
+    op.execute(
+        "ALTER TABLE connected_services.oauth2_clients ALTER COLUMN kind SET DATA TYPE providerkind USING kind::text::providerkind"
+    )
+    op.execute("DROP TYPE providerkind_old CASCADE")
