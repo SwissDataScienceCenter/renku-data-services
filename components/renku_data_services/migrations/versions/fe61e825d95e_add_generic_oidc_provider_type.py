778240b97920c496dd8ceb5343a2c0ac32416914
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2025-09-08 07:45:29 +0000

    update revision

--- components/renku_data_services/migrations/versions/fe61e825d95e_add_generic_oidc_provider_type.py
+++ components/renku_data_services/migrations/versions/fe61e825d95e_add_generic_oidc_provider_type.py
@@ -1,7 +1,7 @@
 """add generic_oidc provider type
 
 Revision ID: fe61e825d95e
-Revises: 35ea9d8f54e8
+Revises: 66e2f1271cf6
 Create Date: 2025-09-05 09:30:23.062585
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "fe61e825d95e"
-down_revision = "35ea9d8f54e8"
+down_revision = "66e2f1271cf6"
 branch_labels = None
 depends_on = None
 
