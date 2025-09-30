27eb916474cb87d3ce53c418fa0a1586daeb7635
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2025-09-30 09:01:52 +0200

    update migrations

---
+++ components/renku_data_services/migrations/versions/97de1962e448_merge_heads.py
@@ -0,0 +1,23 @@
+"""Merge heads
+
+Revision ID: 97de1962e448
+Revises: 53e69e8414ed, 8365db35dc76
+Create Date: 2025-09-30 07:01:28.339163
+
+"""
+
+
+
+# revision identifiers, used by Alembic.
+revision = "97de1962e448"
+down_revision = ("53e69e8414ed", "8365db35dc76")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
