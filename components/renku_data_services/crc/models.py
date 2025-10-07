5e79c19cc27ed0b40bc0dd382f7f9ad5667f1309
Samuel Gaist <samuel.gaist@idiap.ch>
2025-10-07 13:21:35 +0200

    feat: add session ingress class name handling

--- components/renku_data_services/crc/models.py
+++ components/renku_data_services/crc/models.py
@@ -218,6 +218,7 @@ class ClusterSettings:
     session_host: str
     session_port: int
     session_path: str
+    session_ingress_class_name: str | None = None
     session_ingress_annotations: dict[str, Any]
     session_tls_secret_name: str
     session_storage_class: str | None
@@ -233,6 +234,7 @@ class ClusterSettings:
             session_host=self.session_host,
             session_port=self.session_port,
             session_path=self.session_path,
+            session_ingress_class_name=self.session_ingress_class_name,
             session_ingress_annotations=self.session_ingress_annotations,
             session_tls_secret_name=self.session_tls_secret_name,
             session_storage_class=self.session_storage_class,
@@ -361,6 +363,7 @@ class ResourcePool:
                     session_host=tmp["session_host"],
                     session_port=tmp["session_port"],
                     session_path=tmp["session_path"],
+                    session_ingress_class_name=tmp.get("session_ingress_class_name"),
                     session_ingress_annotations=tmp["session_ingress_annotations"],
                     session_tls_secret_name=tmp["session_tls_secret_name"],
                     session_storage_class=tmp["session_storage_class"],
