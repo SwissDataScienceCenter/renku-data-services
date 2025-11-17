88fbe3ee5e00ef2eab2fdfe83397bf0b54a41b73
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2025-11-17 09:28:55 +0100

    feat: indicate supported platforms when checking a session image (#1101)
    Add a new field `platforms` returned when checking a session image with `GET /api/data/sessions/images`. The field may be returned when the image is accessible and will contain the list of supported platforms. If gathering this information fails, the field will simply not be present in the response.

---
+++ components/renku_data_services/notebooks/oci/models.py
@@ -0,0 +1,26 @@
+"""Constants related to OCI images."""
+
+from dataclasses import dataclass
+from enum import StrEnum
+
+
+class ManifestMediaTypes(StrEnum):
+    """The content types related to OCI image manifests."""
+
+    docker_config_v2 = "application/vnd.docker.container.image.v1+json"
+    docker_manifest_v2 = "application/vnd.docker.distribution.manifest.v2+json"
+    docker_list_v2 = "application/vnd.docker.distribution.manifest.list.v2+json"
+    oci_config_v1 = "application/vnd.oci.image.config.v1+json"
+    oci_manifest_v1 = "application/vnd.oci.image.manifest.v1+json"
+    oci_index_v1 = "application/vnd.oci.image.index.v1+json"
+
+
+@dataclass(frozen=True, eq=True, kw_only=True, order=True)
+class Platform:
+    """Represents a runtime platform."""
+
+    os: str
+    architecture: str
+    variant: str | None = None
+    os_version: str | None = None
+    os_features: list[str] | None = None
---
+++ components/renku_data_services/notebooks/oci/utils.py
@@ -0,0 +1,95 @@
+"""Utilities for OCI images."""
+
+from typing import TYPE_CHECKING
+
+import httpx
+
+from renku_data_services import errors
+from renku_data_services.app_config import logging
+from renku_data_services.notebooks.oci.image_config import ImageConfig
+from renku_data_services.notebooks.oci.image_index import ImageIndex
+from renku_data_services.notebooks.oci.image_manifest import ImageManifest
+from renku_data_services.notebooks.oci.models import ManifestMediaTypes, Platform
+
+if TYPE_CHECKING:
+    from renku_data_services.notebooks.api.classes.image import Image, ImageRepoDockerAPI
+
+logger = logging.getLogger(__name__)
+
+
+async def get_image_platforms(
+    manifest_response: httpx.Response, image: "Image", reg_api: "ImageRepoDockerAPI"
+) -> list[Platform] | None:
+    """Returns the list of platforms supported by the image manifest."""
+    try:
+        parsed = parse_manifest_response(manifest_response)
+    except Exception as err:
+        logger.warning(f"Error parsing image manifest: {err}")
+        return None
+
+    if isinstance(parsed, ImageIndex):
+        platforms: list[Platform] = []
+        for manifest in parsed.manifests:
+            # Ignore manifests without a platform
+            if (
+                manifest.platform is None
+                or manifest.platform.os == "unknown"
+                or manifest.platform.architecture == "unknown"
+            ):
+                continue
+            platforms.append(
+                Platform(
+                    architecture=manifest.platform.architecture,
+                    os=manifest.platform.os,
+                    os_features=list(manifest.platform.os_features) if manifest.platform.os_features else None,
+                    os_version=manifest.platform.os_version,
+                    variant=manifest.platform.variant,
+                )
+            )
+        platforms = sorted(set(platforms))
+        return platforms
+
+    try:
+        config_response = await reg_api.get_image_config_from_digest(image=image, config_digest=parsed.config.digest)
+    except Exception as err:
+        logger.warning(f"Error getting image config: {err}")
+        return None
+
+    try:
+        parsed_config = parse_config_response(config_response)
+    except Exception as err:
+        logger.warning(f"Error parsing image config: {err}")
+        return None
+
+    platform = Platform(
+        architecture=parsed_config.architecture,
+        os=parsed_config.os,
+        os_features=list(parsed_config.os_features) if parsed_config.os_features else None,
+        os_version=parsed_config.os_version,
+        variant=parsed_config.variant,
+    )
+    return [platform]
+
+
+def parse_manifest_response(response: httpx.Response) -> ImageIndex | ImageManifest:
+    """Parse a manifest response."""
+    content_type = response.headers.get("Content-Type")
+    if content_type not in {
+        ManifestMediaTypes.docker_list_v2,
+        ManifestMediaTypes.docker_manifest_v2,
+        ManifestMediaTypes.oci_index_v1,
+        ManifestMediaTypes.oci_manifest_v1,
+    }:
+        raise errors.ValidationError(message=f"Unexpected content type {content_type}.")
+
+    if content_type in {ManifestMediaTypes.docker_list_v2, ManifestMediaTypes.oci_index_v1}:
+        return ImageIndex.model_validate_json(response.content)
+    else:
+        return ImageManifest.model_validate_json(response.content)
+
+
+def parse_config_response(response: httpx.Response) -> ImageConfig:
+    """Parse a config response."""
+    # NOTE: it seems that the "Content-Type" is set to "application/octet-stream" by the Docker registry
+    # NOTE: so we do not validate it
+    return ImageConfig.model_validate_json(response.content)
