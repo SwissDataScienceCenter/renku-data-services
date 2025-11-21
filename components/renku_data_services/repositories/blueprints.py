322a3d7ca50c44d8da7bc2f1306c37acfa90db09
Eike Kettner <eike.kettner@sdsc.ethz.ch>
2025-11-21 11:19:26 +0100

    Remove now obsolete repository endpoints

--- components/renku_data_services/repositories/blueprints.py
+++ components/renku_data_services/repositories/blueprints.py
@@ -1,22 +1,18 @@
 """Repositories blueprint."""
 
 from dataclasses import dataclass
-from urllib.parse import unquote
 
 from sanic import HTTPResponse, Request
 from sanic.response import JSONResponse
 
 import renku_data_services.base_models as base_models
-from renku_data_services import errors
 from renku_data_services.base_api.auth import authenticate_2
 from renku_data_services.base_api.blueprint import BlueprintFactoryResponse, CustomBlueprint
 from renku_data_services.base_api.etag import extract_if_none_match
 from renku_data_services.base_models.validation import validated_json
 from renku_data_services.repositories import apispec, models
-from renku_data_services.repositories.apispec_base import RepositoryParams
 from renku_data_services.repositories.db import GitRepositoriesRepository
 from renku_data_services.repositories.git_url import GitUrlError
-from renku_data_services.repositories.utils import probe_repository
 
 
 @dataclass(kw_only=True)
@@ -28,34 +24,6 @@ class RepositoriesBP(CustomBlueprint):
     internal_gitlab_authenticator: base_models.Authenticator
 
     def get_one_repository(self) -> BlueprintFactoryResponse:
-        """Get the metadata available about a repository."""
-
-        @authenticate_2(self.authenticator, self.internal_gitlab_authenticator)
-        @extract_if_none_match
-        async def _get_one_repository(
-            _: Request,
-            user: base_models.APIUser,
-            internal_gitlab_user: base_models.APIUser,
-            repository_url: str,
-            etag: str | None,
-        ) -> JSONResponse | HTTPResponse:
-            repository_url = unquote(repository_url)
-
-            result = await self.git_repositories_repo.get_repository(
-                repository_url=repository_url,
-                user=user,
-                etag=etag,
-                internal_gitlab_user=internal_gitlab_user,
-            )
-            if result.metadata == "Unmodified":
-                return HTTPResponse(status=304)
-            headers = {"ETag": result.metadata.etag} if result.metadata and result.metadata.etag else None
-            body = self._make_result(result)
-            return validated_json(apispec.RepositoryProviderData, body, headers=headers)
-
-        return "/repositories/<repository_url>", ["GET"], _get_one_repository
-
-    def get_one_repository_qp(self) -> BlueprintFactoryResponse:
         """Get the metadata available about a repository."""
 
         @authenticate_2(self.authenticator, self.internal_gitlab_authenticator)
@@ -122,41 +90,3 @@ class RepositoriesBP(CustomBlueprint):
         return apispec.RepositoryProviderData(
             status=status, connection=conn, provider=prov, metadata=meta, error_code=error_code
         )
-
-    def get_one_repository_probe(self) -> BlueprintFactoryResponse:
-        """Probe a repository to check if it is publicly available."""
-
-        async def _get_one_repository_probe(_: Request, repository_url: str) -> HTTPResponse:
-            repository_url = unquote(repository_url)
-            RepositoryParams.model_validate(dict(repository_url=repository_url))
-
-            result = await probe_repository(repository_url)
-
-            if not result:
-                raise errors.MissingResourceError(
-                    message=f"The repository at {repository_url} does not seem to be publicly accessible."
-                )
-            return HTTPResponse(status=200)
-
-        return "/repositories/<repository_url>/probe", ["GET"], _get_one_repository_probe
-
-    def get_one_repository_probe_qp(self) -> BlueprintFactoryResponse:
-        """Probe a repository to check if it is publicly available."""
-
-        async def _get_one_repository_probe(req: Request) -> HTTPResponse:
-            query_args: dict[str, str] = req.get_args() or {}
-            repository_url = query_args.get("url")
-            if repository_url is None:
-                return HTTPResponse(status=404)
-
-            RepositoryParams.model_validate(dict(repository_url=repository_url))
-
-            result = await probe_repository(repository_url)
-
-            if not result:
-                raise errors.MissingResourceError(
-                    message=f"The repository at {repository_url} does not seem to be publicly accessible."
-                )
-            return HTTPResponse(status=200)
-
-        return "/repositories/probe", ["GET"], _get_one_repository_probe
