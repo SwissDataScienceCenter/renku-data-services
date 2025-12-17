f9a2dd8cd0ba20fb3eb32ca3f6a6c7a4694efad7
Eike Kettner <eike.kettner@sdsc.ethz.ch>
2025-12-17 11:08:58 +0100

    Fix alembic

--- components/renku_data_services/migrations/versions/9b18adb58e63_add_hibernation_warning_period.py
+++ components/renku_data_services/migrations/versions/9b18adb58e63_add_hibernation_warning_period.py
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "9b18adb58e63"
-down_revision = "5ec28ea89e0a"
+down_revision = "780302876bce"
 branch_labels = None
 depends_on = None
 
