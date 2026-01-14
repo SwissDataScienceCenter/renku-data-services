6cf939eb3e340147600c699d6fe614bbf1d5d074
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2026-01-14 15:33:05 +0000

    fix

--- components/renku_data_services/migrations/versions/58ad5426c2f3_upgrade_oauth_provider_kind_enum.py
+++ components/renku_data_services/migrations/versions/58ad5426c2f3_upgrade_oauth_provider_kind_enum.py
@@ -7,6 +7,9 @@ Create Date: 2026-01-14 14:35:29.539830
 """
 
 from alembic import op
+from sqlalchemy.exc import OperationalError
+
+from renku_data_services.app_config import logging
 
 # revision identifiers, used by Alembic.
 revision = "58ad5426c2f3"
@@ -14,13 +17,22 @@ down_revision = "9b18adb58e63"
 branch_labels = None
 depends_on = None
 
+logger = logging.getLogger(__name__)
+
 # NOTE: Postgres does not allow removing values from an enum
 
 
 def upgrade() -> None:
-    op.execute("DELETE FROM connected_services.oauth2_clients WHERE kind = 'drive'")
-    op.execute("DELETE FROM connected_services.oauth2_clients WHERE kind = 'onedrive'")
-    op.execute("DELETE FROM connected_services.oauth2_clients WHERE kind = 'dropbox'")
+    connection = op.get_bind()
+    with connection.begin_nested() as tx:
+        try:
+            op.execute("DELETE FROM connected_services.oauth2_clients WHERE kind = 'drive'")
+            op.execute("DELETE FROM connected_services.oauth2_clients WHERE kind = 'onedrive'")
+            op.execute("DELETE FROM connected_services.oauth2_clients WHERE kind = 'dropbox'")
+            tx.commit()
+        except OperationalError as err:
+            logger.debug(f"Skipped DELETE section from migration of the connected_services.oauth2_clients table: {err}")
+            tx.rollback()
     op.execute("ALTER TYPE providerkind RENAME TO providerkind_old")
     op.execute("CREATE TYPE providerkind AS ENUM ('gitlab', 'github', 'google', 'generic_oidc')")
     op.execute(
