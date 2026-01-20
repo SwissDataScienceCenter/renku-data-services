f9775844337a0148327a25ed5a3eaac7c65d314b
Eike Kettner <eike.kettner@sdsc.ethz.ch>
2026-01-20 18:17:30 +0100

    more wip

--- test/components/renku_data_services/resource_usage/test_model.py
+++ test/components/renku_data_services/resource_usage/test_model.py
@@ -1,6 +1,8 @@
 import pytest
+
 from renku_data_services.data_api.dependencies import DependencyManager
-from renku_data_services.resource_usage.core import ResourceRequestsFetch
+from renku_data_services.migrations.core import run_migrations_for_app
+from renku_data_services.resource_usage.core import ResourceRequestsFetch, ResourceRequestsRepo, ResourcesRequestRecorder
 from renku_data_services.resource_usage.model import CpuUsage
 
 
@@ -12,11 +14,15 @@ def test_cpu_usage() -> None:
 
 @pytest.mark.asyncio
 async def test_play(app_manager_instance: DependencyManager) -> None:
+    run_migrations_for_app("common")
+
+    #    print(app_manager_instance.k8s_client.__clients)
 
     rcd = ResourceRequestsFetch(app_manager_instance.k8s_client)
-    result = await rcd.get_resources_requests()
-    print(result)
-    # data = rcd.test()
-    # print()
-    # for _,e in data.items():
-    #     print(e)
+    repo = ResourceRequestsRepo(app_manager_instance.config.db.async_session_maker)
+    recorder = ResourcesRequestRecorder(repo, rcd)
+    await recorder.record_resource_requests("renku")
+
+    all = repo.find_all()
+    async for e in all:
+        print(e)
