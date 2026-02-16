77d05a4094fc1d869c8a262dad3d9315610597c8
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2026-02-16 13:01:59 +0000

    removeme: merge migrations heads

---
+++ components/renku_data_services/migrations/versions/0bfc18c91b05_removeme.py
@@ -0,0 +1,21 @@
+"""removeme: merge migrations heads
+
+Revision ID: 0bfc18c91b05
+Revises: 287879848fb3, fddfe7960a8b
+Create Date: 2026-02-16 12:37:55.425957
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "0bfc18c91b05"
+down_revision = ("287879848fb3", "fddfe7960a8b")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
