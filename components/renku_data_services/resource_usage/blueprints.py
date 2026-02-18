e80abe8e88a49a6d032c024c77a56d8223220e91
Eike Kettner <eike.kettner@sdsc.ethz.ch>
2026-02-18 17:02:33 +0100

    endpoints are responding

--- components/renku_data_services/resource_usage/blueprints.py
+++ components/renku_data_services/resource_usage/blueprints.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass
 
+from renku_data_services.base_models.validation import validated_json
 from sanic import HTTPResponse, Request, empty, json
+from sanic.response import JSONResponse
 from sanic_ext import validate
 
 from renku_data_services import base_models, errors
 from renku_data_services.base_api.auth import authenticate, only_admins
 from renku_data_services.base_api.blueprint import CustomBlueprint
-from renku_data_services.base_api.misc import BlueprintFactoryResponse
+from renku_data_services.base_api.misc import BlueprintFactoryResponse, validate_db_ids
 from renku_data_services.resource_usage import apispec, model
 from renku_data_services.resource_usage.db import ResourceRequestsRepo
 
@@ -15,7 +17,7 @@ def validate_resource_pool_limit_put(id: int, body: apispec.ResourcePoolLimitPut
     """Validate resource pool limit."""
     if body.user_limit > body.total_limit:
         raise errors.ValidationError(
-            message=f"The user_limit '{body.user_limit}' must be lower than total_limit '{body.total_limit}f'.",
+            message=f"The user_limit '{body.user_limit}' must be lower than total_limit '{body.total_limit}'.",
         )
     return model.ResourcePoolLimits(id, model.Credit.from_int(body.total_limit), model.Credit.from_int(body.user_limit))
 
@@ -27,11 +29,12 @@ class ResourceUsageBP(CustomBlueprint):
     rr_repo: ResourceRequestsRepo
     authenticator: base_models.Authenticator
 
-    def put(self) -> BlueprintFactoryResponse:
+    def put_pool_limits(self) -> BlueprintFactoryResponse:
         """Set resource pool limits."""
 
         @authenticate(self.authenticator)
         @only_admins
+        @validate_db_ids
         @validate(json=apispec.ResourcePoolLimitPut)
         async def _put(
             _: Request, user: base_models.APIUser, resource_pool_id: int, body: apispec.ResourcePoolLimitPut
@@ -40,17 +43,19 @@ class ResourceUsageBP(CustomBlueprint):
             await self.rr_repo.set_resource_pool_limits(limits)
             return empty()
 
-        return "/resource_usage/pool_limits/<pool_id>", ["PUT"], _put
+        return "/resource_usage/pool_limits/<resource_pool_id>", ["PUT"], _put
 
-    def get(self) -> BlueprintFactoryResponse:
+    def get_pool_limits(self) -> BlueprintFactoryResponse:
         """Get resource pool limits."""
 
         @authenticate(self.authenticator)
         @only_admins
-        async def _get(_: Request, user: base_models.APIUser, pool_id: int) -> HTTPResponse:
-            result = await self.rr_repo.find_resource_pool_limits(pool_id)
+        @validate_db_ids
+        async def _get(_: Request, user: base_models.APIUser, resource_pool_id: int) -> JSONResponse:
+            result = await self.rr_repo.find_resource_pool_limits(resource_pool_id)
             if result:
-                return json(
+                return validated_json(
+                    apispec.ResourcePoolLimits,
                     apispec.ResourcePoolLimits(
                         total_limit=result.total_limit.value, user_limit=result.user_limit.value, pool_id=result.pool_id
                     )
@@ -58,4 +63,16 @@ class ResourceUsageBP(CustomBlueprint):
             else:
                 raise errors.MissingResourceError()
 
-        return "/resource_usage/pool_limits/<pool_id>", ["GET"], _get
+        return "/resource_usage/pool_limits/<resource_pool_id>", ["GET"], _get
+
+    def delete_pool_limits(self) -> BlueprintFactoryResponse:
+        """Delete defined pool limits."""
+
+        @authenticate(self.authenticator)
+        @only_admins
+        @validate_db_ids
+        async def _delete(_: Request, user: base_models.APIUser, resource_pool_id: int) -> HTTPResponse:
+            await self.rr_repo.delete_resource_pool_limits(resource_pool_id)
+            return empty()
+
+        return "/resource_usage/pool_limits/<resource_pool_id>", ["DELETE"], _delete
