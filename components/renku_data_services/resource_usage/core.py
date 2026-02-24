fd8c928a9996f7d51811026926a6278499c52737
Eike Kettner <eike.kettner@sdsc.ethz.ch>
2026-02-24 16:28:07 +0100

    pod should be capitalized

--- components/renku_data_services/resource_usage/core.py
+++ components/renku_data_services/resource_usage/core.py
@@ -41,7 +41,7 @@ class ResourceRequestsFetch(ResourceRequestsFetchProto):
         date = datetime.now(UTC).replace(microsecond=0)
         result: dict[str, ResourcesRequest] = {}
 
-        pod_filter = K8sObjectFilter(gvk=GVK(kind="pod", version="v1"))
+        pod_filter = K8sObjectFilter(gvk=GVK(kind="Pod", version="v1"))
         pvc_filter = K8sObjectFilter(gvk=GVK(kind="PersistentVolumeClaim", version="v1"))
 
         node_cache: dict[str, ResourceDataFacade] = {}
