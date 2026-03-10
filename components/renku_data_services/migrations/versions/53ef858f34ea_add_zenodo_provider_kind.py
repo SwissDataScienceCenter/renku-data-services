b1df194be4185556b4df48564708f4663f5a9e5f
Tasko Olevski <16360283+olevski@users.noreply.github.com>
2026-03-10 09:03:37 +0100

    feat: add k8s client for deposits (#1222)

---
+++ components/renku_data_services/migrations/versions/53ef858f34ea_add_zenodo_provider_kind.py
@@ -0,0 +1,30 @@
+"""add zenodo provider kind
+
+Revision ID: 53ef858f34ea
+Revises: c6af6a1088f1
+Create Date: 2026-02-20 16:45:47.200173
+
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "53ef858f34ea"
+down_revision = "c6af6a1088f1"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute("ALTER TYPE providerkind ADD VALUE 'zenodo'")
+
+
+def downgrade() -> None:
+    # NOTE: Postgres does not allow removing values from an enum
+    op.execute("DELETE FROM connected_services.oauth2_clients WHERE kind = 'zenodo'")
+    op.execute("ALTER TYPE providerkind RENAME TO providerkind_old;")
+    op.execute("CREATE TYPE providerkind AS ENUM ('dropbox', 'generic_oidc', 'github', 'gitlab', 'google')")
+    op.execute(
+        "ALTER TABLE connected_services.oauth2_clients ALTER COLUMN kind SET DATA TYPE providerkind USING kind::text::providerkind"
+    )
+    op.execute("DROP TYPE providerkind_old CASCADE")
