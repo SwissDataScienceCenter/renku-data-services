79bc075e197298800279c24e65fc35dcfe3676fa
Eike Kettner <eike.kettner@sdsc.ethz.ch>
2026-04-01 09:13:26 +0200

    log session culling config

--- components/renku_data_services/notebooks/core_sessions.py
+++ components/renku_data_services/notebooks/core_sessions.py
@@ -1057,7 +1057,7 @@ async def start_session(
             template=Template(metadata=AmaltheaMetadata(annotations=annotations, labels=labels)),
         ),
     )
-    logger.info(f"Push Session: {session}")
+    logger.info(f"Push Session Culling: {session.spec.culling}")
     secrets_to_create = session_extras.secrets or []
     for s in secrets_to_create:
         logger.debug(f"Creating {len(secrets_to_create)} session secrets")
