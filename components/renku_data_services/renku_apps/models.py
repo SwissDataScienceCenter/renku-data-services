b76a4168b5dff682d6e9e72ce4bd1d85099ae869
Wes Johnson <wes@wes-johnson.com>
2026-05-12 13:24:28 +0200

    api spec and models

---
+++ components/renku_data_services/renku_apps/models.py
@@ -0,0 +1,41 @@
+"""Models for apps."""
+
+from dataclasses import dataclass
+from datetime import datetime
+from enum import StrEnum
+
+from ulid import ULID
+
+
+class AppState(StrEnum):
+    """The status of an app."""
+
+    PENDING = "pending"
+    READY = "ready"
+    FAILED = "failed"
+
+
+@dataclass(frozen=True, eq=True, kw_only=True)
+class UnsavedApp:
+    """An unsaved app."""
+
+    project_id: ULID
+    image: str
+    created_by: str
+    status: AppState = AppState.PENDING
+
+
+@dataclass(frozen=True, eq=True, kw_only=True)
+class App(UnsavedApp):
+    """An app stored in the database."""
+
+    id: ULID
+    created_at: datetime
+    updated_at: datetime
+
+
+@dataclass(frozen=True, eq=True, kw_only=True)
+class AppPatch:
+    """A patch for an existing app."""
+
+    image: str | None = None
