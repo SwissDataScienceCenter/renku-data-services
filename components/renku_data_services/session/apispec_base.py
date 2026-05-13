f6a2dcfc4665575e1c606a86ff71e1d0735caaca
Eike Kettner <eike.kettner@sdsc.ethz.ch>
2026-05-13 11:23:29 +0200

    Fix serializing (2)

--- components/renku_data_services/session/apispec_base.py
+++ components/renku_data_services/session/apispec_base.py
@@ -6,7 +6,7 @@ from typing import Any
 from pydantic import BaseModel, ConfigDict, field_validator
 from ulid import ULID
 
-from renku_data_services.session import apispec, models
+from renku_data_services.session import models
 
 
 class BaseAPISpec(BaseModel):
@@ -55,11 +55,11 @@ class BaseAPISpec(BaseModel):
             return val.as_posix()
         return val
 
-    @field_validator("launcher_type", mode="before")
+    @field_validator("launcher_type", mode="before", check_fields=False)
     @classmethod
-    def convert_to_api_launcher_type(cls, lt: models.LauncherType) -> apispec.LauncherType:
+    def convert_to_api_launcher_type(cls, lt: models.LauncherType) -> str:
         """Convert the model launcher type into a valid value when serializing."""
         if lt == models.LauncherType.non_interactive:
-            return apispec.LauncherType.non_interactive
+            return "non-interactive"
         else:
-            return apispec.LauncherType.interactive
+            return "interactive"
