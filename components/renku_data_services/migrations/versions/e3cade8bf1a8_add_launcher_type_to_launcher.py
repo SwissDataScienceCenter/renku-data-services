d4626316dd0c6e3af3f612e7f90308c1bed06b57
Eike Kettner <eike.kettner@sdsc.ethz.ch>
2026-05-20 13:04:04 +0200

    fix alembic down revision

--- components/renku_data_services/migrations/versions/e3cade8bf1a8_add_launcher_type_to_launcher.py
+++ components/renku_data_services/migrations/versions/e3cade8bf1a8_add_launcher_type_to_launcher.py
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "e3cade8bf1a8"
-down_revision = "cd424c01676e"
+down_revision = "c29f65247e88"
 branch_labels = None
 depends_on = None
 
