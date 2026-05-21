f5b5b5c1cac0a1a9841f19d70ed80c472fd417e8
Wes Johnson <wes@wes-johnson.com>
2026-05-21 10:46:06 +0200

    make url nullable

--- components/renku_data_services/renku_apps/models.py
+++ components/renku_data_services/renku_apps/models.py
@@ -6,6 +6,8 @@ from enum import StrEnum
 
 from ulid import ULID
 
+from renku_data_services.renku_apps import apispec
+
 
 class AppStatus(StrEnum):
     """The status of an app."""
@@ -26,3 +28,15 @@ class App:
     url: str | None = None
     started: datetime | None = None
     image: str | None = None
+
+    def as_apispec(self) -> apispec.AppResponse:
+        """Convert the app to an API response model."""
+        return apispec.AppResponse(
+            name=self.name,
+            launcher_id=str(self.launcher_id),
+            status=self.status,
+            url=self.url,
+            project_id=str(self.project_id),
+            started=self.started,
+            image=self.image,
+        )
