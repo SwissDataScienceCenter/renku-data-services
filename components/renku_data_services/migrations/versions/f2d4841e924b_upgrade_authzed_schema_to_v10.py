9f8bc4e2efd21ef8b13221676c3945f43cee1e11
Tasko Olevski <16360283+olevski@users.noreply.github.com>
2026-05-29 17:15:26 +0200

    chore: rebase the migrations

--- components/renku_data_services/migrations/versions/f2d4841e924b_upgrade_authzed_schema_to_v10.py
+++ components/renku_data_services/migrations/versions/f2d4841e924b_upgrade_authzed_schema_to_v10.py
@@ -1,20 +1,20 @@
-"""upgrade authzed schema to v10
+"""upgrade authzed schema to v11
 
 Revision ID: f2d4841e924b
-Revises: a8f0e7b3c2d1
+Revises: 65f8330c4d25
 Create Date: 2026-05-27 14:32:12.144808
 
 """
 
 from renku_data_services.app_config import logging
 from renku_data_services.authz.config import AuthzConfig
-from renku_data_services.authz.schemas import v10
+from renku_data_services.authz.schemas import v11
 
 logger = logging.getLogger(__name__)
 
 # revision identifiers, used by Alembic.
 revision = "f2d4841e924b"
-down_revision = "a8f0e7b3c2d1"
+down_revision = "65f8330c4d25"
 branch_labels = None
 depends_on = None
 
@@ -22,16 +22,16 @@ depends_on = None
 def upgrade() -> None:
     config = AuthzConfig.from_env()
     client = config.authz_client()
-    responses = v10.upgrade(client)
+    responses = v11.upgrade(client)
     logger.info(
-        f"Finished upgrading the Authz schema to version 10 in Alembic revision {revision}, response: {responses}"
+        f"Finished upgrading the Authz schema to version 11 in Alembic revision {revision}, response: {responses}"
     )
 
 
 def downgrade() -> None:
     config = AuthzConfig.from_env()
     client = config.authz_client()
-    responses = v10.downgrade(client)
+    responses = v11.downgrade(client)
     logger.info(
-        f"Finished downgrading the Authz schema from version 10 in Alembic revision {revision}, response: {responses}"
+        f"Finished downgrading the Authz schema from version 11 in Alembic revision {revision}, response: {responses}"
     )
