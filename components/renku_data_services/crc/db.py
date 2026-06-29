820a84d708c6b3f14beb4524b31b3363fe5fe291
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2026-06-29 14:18:47 +0000

    fix dump after quota remove

--- components/renku_data_services/crc/db.py
+++ components/renku_data_services/crc/db.py
@@ -639,6 +639,7 @@ class ResourcePoolRepository(_Base):
                 # Remove the existing quota
                 await self.quotas_repo.delete_quota(name=rp.quota, cluster_id=cluster_id)
                 rp.quota = None
+                quota = None
             elif isinstance(update.quota, models.QuotaPatch) and rp.quota is None:
                 # Create a new quota, the `update.quota` object has already been validated
                 assert update.quota.cpu is not None
