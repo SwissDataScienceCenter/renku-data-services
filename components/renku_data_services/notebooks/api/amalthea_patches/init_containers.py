2e6b710736e01893417ca5936a7b882b04822b88
Tasko Olevski <16360283+olevski@users.noreply.github.com>
2026-07-08 15:41:18 +0200

    squashme: minor fix

--- components/renku_data_services/notebooks/api/amalthea_patches/init_containers.py
+++ components/renku_data_services/notebooks/api/amalthea_patches/init_containers.py
@@ -35,6 +35,7 @@ async def git_clone_container_v2(
     repositories: list[Repository],
     git_providers: list[GitProvider],
     workspace_mount_path: PurePosixPath,
+    work_dir: PurePosixPath,
     lfs_auto_fetch: bool = False,
     uid: int = 1000,
     gid: int = 1000,
@@ -51,11 +52,19 @@ async def git_clone_container_v2(
         read_only_etc_certs=True,
     )
 
+    # Trying to use work_dir as the place to clone is in place for backwards compatibility.
+    # We never had issues because before introducing jobs because we always set the work dir as a child of
+    # the storage mount path. But for running jobs with a custom image, then the work dir is usually not
+    # set to anything and defaults to `/`. In this case the work dir is not a child of the storage
+    # mount path and it is not writable in many cases.
+    git_clone_mount_path = work_dir
+    if not work_dir.relative_to(workspace_mount_path):
+        git_clone_mount_path = workspace_mount_path
     prefix = "GIT_CLONE_"
     env = [
         {
             "name": f"{prefix}MOUNT_PATH",
-            "value": workspace_mount_path.as_posix(),
+            "value": git_clone_mount_path.as_posix(),
         },
         {
             "name": f"{prefix}LFS_AUTO_FETCH",
--- components/renku_data_services/notebooks/core_sessions.py
+++ components/renku_data_services/notebooks/core_sessions.py
@@ -129,6 +129,7 @@ async def get_extra_init_containers(
     repositories: list[Repository],
     git_providers: list[GitProvider],
     storage_mount: PurePosixPath,
+    work_dir: PurePosixPath,
     uid: int = 1000,
     gid: int = 1000,
 ) -> SessionExtraResources:
@@ -143,6 +144,7 @@ async def get_extra_init_containers(
         repositories=repositories,
         git_providers=git_providers,
         workspace_mount_path=storage_mount,
+        work_dir=work_dir,
         uid=uid,
         gid=gid,
     )
@@ -967,6 +969,7 @@ async def start_session(
             repositories,
             git_providers,
             storage_mount,
+            work_dir,
             uid=environment.uid,
             gid=environment.gid,
         )
@@ -1423,6 +1426,7 @@ async def patch_session(
             repositories,
             git_providers,
             storage_mount,
+            work_dir,
             uid=environment.uid,
             gid=environment.gid,
         )
