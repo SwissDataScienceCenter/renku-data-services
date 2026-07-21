ab7beb0d95c58a32c44394481128a999c243b422
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2026-07-21 07:40:17 +0000

    fix ULID serialization

--- components/renku_data_services/persisted_logs/apispec_base.py
+++ components/renku_data_services/persisted_logs/apispec_base.py
@@ -1,6 +1,9 @@
 """Base models for API specifications."""
 
-from pydantic import BaseModel, ConfigDict
+from typing import Any
+
+from pydantic import BaseModel, ConfigDict, field_validator
+from ulid import ULID
 
 
 class BaseAPISpec(BaseModel):
@@ -10,3 +13,11 @@ class BaseAPISpec(BaseModel):
         # Enables orm mode for pydantic."""
         from_attributes=True,
     )
+
+    @field_validator("*", mode="before", check_fields=False)
+    @classmethod
+    def serialize_ulid(cls, value: Any) -> Any:
+        """Handle ULIDs."""
+        if isinstance(value, ULID):
+            return str(value)
+        return value
