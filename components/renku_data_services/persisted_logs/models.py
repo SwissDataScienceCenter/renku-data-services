14d9d61a9466fa8ba9bdf5b42f95011f10fec13f
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2026-07-30 11:33:06 +0000

    remove unused models

--- components/renku_data_services/persisted_logs/models.py
+++ components/renku_data_services/persisted_logs/models.py
@@ -5,25 +5,6 @@ from dataclasses import dataclass
 
 from ulid import ULID
 
-# @dataclass(eq=True, frozen=True, kw_only=True)
-# class UnsavedLogLine:
-#     """Represents an unsaved log line."""
-
-#     id: str
-#     """The ID of the log line.
-
-#     This is used to de-duplicate log lines.
-#     """
-
-#     user_id: str
-#     run_id: ULID
-#     session_uid: str | None
-#     launcher_id: ULID
-#     submission_id: str | None
-#     container: str
-#     timestamp: int
-#     log_line: str
-
 
 @dataclass(eq=True, frozen=True, kw_only=True)
 class SessionRun:
@@ -54,27 +35,3 @@ class PersistedSessionLogs:
 
     run: SessionRun
     logs: SessionRunLogs
-
-
-# @dataclass(eq=True, frozen=True, kw_only=True)
-# class InsertLogsResult:
-#     """Result of inserting a log stream in the database."""
-
-#     log_count: int
-#     last_timestamp: int
-
-
-# @dataclass(eq=True, frozen=True, kw_only=True)
-# class UnsavedBuildLogLine:
-#     """Represents an unsaved image build log line."""
-
-#     id: str
-#     """The ID of the log line.
-
-#     This is used to de-duplicate log lines.
-#     """
-
-#     build_id: ULID
-#     container: str
-#     timestamp: int
-#     log_line: str
