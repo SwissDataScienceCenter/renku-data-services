bd444357c330937e181b849f0252538f685158d7
Tasko Olevski <16360283+olevski@users.noreply.github.com>
2026-08-01 09:28:27 +0200

    chore: centralize rclone config handling

--- components/renku_data_services/notebooks/data_sources.py
+++ components/renku_data_services/notebooks/data_sources.py
@@ -38,7 +38,7 @@ from renku_data_services.users.db import UserRepo
 from renku_data_services.utils.cryptography import get_encryption_key
 
 if TYPE_CHECKING:
-    from renku_data_services.storage.models import RCloneConfig
+    from renku_data_services.storage.rclone import RCloneConfig
 
 logger = logging.getLogger(__name__)
 
@@ -142,7 +142,7 @@ class DataSourceRepository:
         configuration = dict(items)
         if configuration.get("type") != data_connector.storage.configuration.get("type"):
             logger.warning(
-                f"Data connector type changed to {data_connector.storage.configuration.get("type")}, skipping!"
+                f"Data connector type changed to {data_connector.storage.configuration.get('type')}, skipping!"
             )
             return None
 
@@ -221,7 +221,7 @@ class DataSourceRepository:
         if provider is None:
             logger.info(
                 f"Skipping data connector {str(data_connector.id)} of type "
-                f"{data_connector.storage.configuration["type"]} "
+                f"{data_connector.storage.configuration['type']} "
                 f"because no provider of kind {provider_kind.value} was found."
             )
             return None
@@ -229,7 +229,7 @@ class DataSourceRepository:
         if connection is None:
             logger.info(
                 f"Skipping data connector {str(data_connector.id)} of type "
-                f"{data_connector.storage.configuration["type"]} "
+                f"{data_connector.storage.configuration['type']} "
                 f"because no active connection was found; user needs to connect with {provider.provider.id}."
             )
             return None
@@ -237,7 +237,7 @@ class DataSourceRepository:
         if not token_set or not token_set.access_token:
             logger.info(
                 f"Skipping data connector {str(data_connector.id)} of type "
-                f"{data_connector.storage.configuration["type"]} "
+                f"{data_connector.storage.configuration['type']} "
                 f"because the connection is not active; user needs to re-connect with {provider.provider.id}."
             )
             return None
