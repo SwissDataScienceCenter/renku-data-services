d7ce4e7e2bd698128998200c149e09730811a926
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2026-08-05 11:32:37 +0000

    wip: fix migrations

--- components/renku_data_services/migrations/versions/140a76cc492c_wip_merge.py
+++ components/renku_data_services/migrations/versions/140a76cc492c_wip_merge.py
@@ -1,14 +1,14 @@
 """wip_merge
 
 Revision ID: 140a76cc492c
-Revises: 36435401b2e7
+Revises: eadfb5e7e7cb
 Create Date: 2026-08-05 11:20:06.820853
 
 """
 
 # revision identifiers, used by Alembic.
 revision = "140a76cc492c"
-down_revision = ("f65223ca0978", "36435401b2e7")
+down_revision = "eadfb5e7e7cb"
 branch_labels = None
 depends_on = None
 
