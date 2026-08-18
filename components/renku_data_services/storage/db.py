35843e6801f170af18777588989fe607c76b10d9
Eike Kettner <eike.kettner@sdsc.ethz.ch>
2026-08-18 18:22:12 +0200

    style fix

--- components/renku_data_services/storage/db.py
+++ components/renku_data_services/storage/db.py
@@ -227,9 +227,7 @@ class ProjectStorageRepository:
         if existing.scalar():
             raise errors.ValidationError(message=f"Project {input.project_id} is already in the allow list.")
 
-        existing_project = await session.execute(
-            select(exists().where(schemas.ProjectORM.id == input.project_id))
-        )
+        existing_project = await session.execute(select(exists().where(schemas.ProjectORM.id == input.project_id)))
         if not existing_project.scalar():
             raise errors.MissingResourceError(message=f"The project {input.project_id} doesn't exist.")
 
