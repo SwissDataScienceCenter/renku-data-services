bc8f2db6939543f1ae5c3bc0efc3a140fc3ea011
Salim Kayal <salim.kayal@idiap.ch>
2026-09-14 21:22:27 +0200

    refactor: consistency

--- bases/renku_data_services/data_api/dependencies.py
+++ bases/renku_data_services/data_api/dependencies.py
@@ -86,7 +86,6 @@ from renku_data_services.secrets.db import LowLevelUserSecretsRepo, UserSecretsR
 from renku_data_services.session.constants import BUILD_RUN_GVK, TASK_RUN_GVK
 from renku_data_services.session.db import SessionRepository
 from renku_data_services.session.k8s_client import ShipwrightClient
-from renku_data_services.ssh_proxy.blueprints import SSHProxyBP
 from renku_data_services.ssh_proxy.constants import SSH_PROXY_SCOPE
 from renku_data_services.storage.db import ProjectStorageRepository
 from renku_data_services.storage.project_storage_k8s import ProjectStorageK8s
@@ -196,7 +195,6 @@ class DependencyManager:
     internal_scope_verifier: ScopeVerifier
     project_storage_k8s: ProjectStorageK8s
     project_storage_repo: ProjectStorageRepository
-    ssh_proxy: SSHProxyBP
 
     spec: dict[str, Any] = field(init=False, repr=False, default_factory=dict)
     app_name: str = "renku_data_services"
@@ -441,13 +439,6 @@ class DependencyManager:
             user_preferences_config=config.user_preferences,
         )
         ssh_key_repo = SSHKeyRepository(session_maker=config.db.async_session_maker)
-        ssh_proxy = SSHProxyBP(
-            name="ssh_proxy",
-            url_prefix="/api/data",
-            service_authenticator=service_authenticator,
-            ssh_key_repo=ssh_key_repo,
-            nb_config=config.nb_config,
-        )
         low_level_user_secrets_repo = LowLevelUserSecretsRepo(
             session_maker=config.db.async_session_maker,
         )
@@ -596,5 +587,4 @@ class DependencyManager:
             internal_scope_verifier=internal_scope_verifier,
             project_storage_k8s=project_storage_k8s,
             project_storage_repo=project_storage_repo,
-            ssh_proxy=ssh_proxy,
         )
