d807f8cf4fe48e81077d5e4bd184bd80287e07cb
Salim Kayal <salim.kayal@idiap.ch>
2026-09-14 15:09:33 +0200

    feat(authn): support service-scoped internal tokens for the ssh proxy

--- bases/renku_data_services/data_api/dependencies.py
+++ bases/renku_data_services/data_api/dependencies.py
@@ -85,6 +85,7 @@ from renku_data_services.secrets.db import LowLevelUserSecretsRepo, UserSecretsR
 from renku_data_services.session.constants import BUILD_RUN_GVK, TASK_RUN_GVK
 from renku_data_services.session.db import SessionRepository
 from renku_data_services.session.k8s_client import ShipwrightClient
+from renku_data_services.ssh_proxy.constants import SSH_PROXY_SCOPE
 from renku_data_services.storage.db import ProjectStorageRepository
 from renku_data_services.storage.project_storage_k8s import ProjectStorageK8s
 from renku_data_services.storage.rclone import RCloneValidator
@@ -141,6 +142,7 @@ class DependencyManager:
     authenticator: base_models.Authenticator
     gitlab_authenticator: base_models.Authenticator
     internal_authenticator: RenkuSelfAuthenticator
+    service_authenticator: RenkuSelfAuthenticator
     quota_repo: QuotaRepository
     gitlab_client: base_models.GitlabAPIProtocol
     kc_api: IKeycloakAPI
@@ -368,6 +370,9 @@ class DependencyManager:
                 )
 
         internal_authenticator = RenkuSelfAuthenticator.from_config(config=config.internal_authn_config)
+        service_authenticator = RenkuSelfAuthenticator.from_config(
+            config.internal_authn_config, required_scope=SSH_PROXY_SCOPE
+        )
         internal_token_mint = RenkuSelfTokenMint.from_config(config=config.internal_authn_config)
         internal_scope_verifier = ScopeVerifier(
             deposit_config=config.deposit_config,
@@ -527,6 +532,7 @@ class DependencyManager:
             authenticator=authenticator,
             gitlab_authenticator=gitlab_authenticator,
             internal_authenticator=internal_authenticator,
+            service_authenticator=service_authenticator,
             gitlab_client=gitlab_client,
             user_store=user_store,
             quota_repo=quota_repo,
