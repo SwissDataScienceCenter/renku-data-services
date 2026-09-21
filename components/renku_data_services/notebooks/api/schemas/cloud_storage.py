e48c0b733c93d87514c1abe7c9b7593773d38a29
Tasko Olevski <16360283+olevski@users.noreply.github.com>
2026-09-21 19:48:35 +0200

    chore: address comments

--- components/renku_data_services/notebooks/api/schemas/cloud_storage.py
+++ components/renku_data_services/notebooks/api/schemas/cloud_storage.py
@@ -140,9 +140,6 @@ class RCloneStorage(ICloudStorageRequest):
             "remote": self.name or base_name,
             "remotePath": self.source_path,
             # NOTE: The sensitive fields are injected separately if the user secret key for decrypting is present.
-            # We don't want them stored in plain text in the Kubernetes secret the secrets are saved in the DB.
-            # If the secrets are not saved in the DB and are provided by the user when the session starts up,
-            # then they are saved in plain text in the k8s secret.
             "configData": rc.config_string(
                 name=self.name or base_name, keep_sensitive=user_secret_key is None and self.user_secret_key is None
             ),
